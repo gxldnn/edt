790c0596f84de2d0321557b59fa41ac6ae0b607e
gxldnn <138344266+gxldnn@users.noreply.github.com>
2024-11-27 13:01:46 +0100

    Update auto.sh

--- ASGBD(M10)/drawio/auto.sh
+++ ASGBD(M10)/drawio/auto.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 function ver (){
 
-    if [[ $1 == "bases" ]];then
+    if [[ $1 == "items" ]];then
         clear
-        echo -ne "Que base de datos quierers ver?"
+        echo -ne "Que base de datos quieres ver?"
         read -p "> " db
         watch -n 0.5 "psql -U isard -d $db -c '\d'"
-    elif [[ $1 ==  "tabla" ]];then
+    elif [[ $1 ==  "table" ]];then
         clear
-        echo -ne "Que base de datos quierers ver?"
+        echo -ne "Que base de datos quieres ver?"
         read -p "> " db
         clear
         echo -e "Que tabla quieres ver?"
         read -p "> " table
         watch -n 0.5 "psql -U isard -d $db -c '\d $table'"
+    elif [[ $1 == "-h" ]];then
+        echo -n "Do ""auto.sh items"" to see objets from a database"
+        echo -n "Do ""auto.sh table"" to see a specific table from a database"
     fi
 }
 ver $1
+

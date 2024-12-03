07784f3bdbdffaaef22abaf2431312ef48802c3d
gxldnn <riberajan@gmail.com>
2024-12-03 12:47:48 +0100

    4Carreteres SQL

---
+++ ASGBD(M10)/drawio/4_carreteres/4_carreteres.sql
@@ -0,0 +1,49 @@
+\c template1
+DROP DATABASE IF EXISTS carreteres;
+CREATE DATABASE carreteres;
+\c carreteres
+
+CREATE TABLE carretera (
+    idcarretera SMALLINT,
+    nom varchar(20),
+    ubicacio varchar(20),
+    longitut varchar(20),
+    CONSTRAINT carretera_idcarretera_pk PRIMARY KEY (idcarretera)
+);
+
+CREATE TABLE area (
+    idarea SMALLINT,
+    nom varchar(20),
+    ubicacio varchar(20),
+    CONSTRAINT area_idarea_pk PRIMARY KEY (idarea)
+);
+
+
+CREATE TABLE termemunicipal (
+    idterme SMALLINT,
+    nom varchar(20),
+    comarca varchar(20),
+    CONSTRAINT termemunicipal_idterme_pk PRIMARY KEY (idterme)
+);
+
+CREATE TABLE tram (
+    idtram SMALLINT,
+    nom varchar(20),
+    kilometresortida varchar(20),
+    idcarretera SMALLINT,
+    idarea SMALLINT,
+    CONSTRAINT tram_idtram_pk PRIMARY KEY (idtram),
+    CONSTRAINT tram_idcarretera_fk FOREIGN KEY (idcarretera) REFERENCES carretera(idcarretera),
+    CONSTRAINT tram_idarea_fk FOREIGN KEY (idarea) REFERENCES area(idarea)
+);
+
+CREATE TABLE tramxterme (
+    idtram SMALLINT,
+    idterme SMALLINT,
+    kmentrada varchar(40),
+    kmsortida varchar(40),
+    CONSTRAINT tramxterme_idtram_idterme_pk PRIMARY KEY (idtram,idterme),
+    CONSTRAINT tramxterme_idtram_fk FOREIGN KEY (idtram) REFERENCES tram(idtram),
+    CONSTRAINT tramxterme_idterme_fk FOREIGN KEY (idterme) REFERENCES termemunicipal(idterme)
+);
+

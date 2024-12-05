e1e45f378562bf21b657435466896fc9473b38c3
gxldnn <riberajan@gmail.com>
2024-12-05 10:27:09 +0100

    asd

--- ASGBD(M10)/drawio/6_museus/6_museus.sql
+++ ASGBD(M10)/drawio/6_museus/6_museus.sql
@@ -3,6 +3,31 @@ DROP DATABASE IF EXISTS museu;
 CREATE DATABASE museu;
 \c museu
 
+-- Jerarquia
+CREATE TABLE format (
+    idformat SMALLINT,
+    descripcio VARCHAR(255) NOT NULL,
+    CONSTRAINT format_idformat_pk PRIMARY KEY (idformat)
+);
+
+CREATE TABLE tipuspintura (
+    idtipuspintura SMALLINT,
+    descripcio VARCHAR(255) NOT NULL,
+    CONSTRAINT tipuspintura_idtipuspintura_pk PRIMARY KEY (idtipuspintura)
+);
+
+CREATE TABLE material (
+    idmaterial SMALLINT,
+    descripcio VARCHAR(255) NOT NULL,
+    CONSTRAINT material_idmaterial_pk PRIMARY KEY (idmaterial)
+);
+
+CREATE TABLE estil (
+    idestil SMALLINT,
+    descripcio VARCHAR(255) NOT NULL,
+    CONSTRAINT estil_idestil_pk PRIMARY KEY (idestil)
+);
+
 CREATE TABLE museu (
     idmuseu SMALLINT,
     nom varchar(20),
@@ -35,5 +60,31 @@ CREATE TABLE exposicio (
 
 CREATE TABLE obra (
     idobra SMALLINT,
-    tipus
+    tipus SMALLINT,
+    idformat SMALLINT,
+    idtipuspintura SMALLINT,
+    idmaterial SMALLINT,
+    idestil SMALLINT,
+    idmuseu SMALLINT,
+    idsala SMALLINT,
+    CONSTRAINT obra_idobra_pk PRIMARY KEY,
+    CONSTRAINT obra_idmuseuidsala_fkc FOREIGN KEY (idsala,idmuseu) REFERENCES sala(idsala,idmuseu) -- FK composta
+    CONSTRAINT obra_idformat_fk FOREIGN KEY (idformat) REFERENCES format(idformat),
+    CONSTRAINT obra_idtipuspintura_fk FOREIGN KEY (idtipuspintura) REFERENCES TipusPintura(idtipuspintura),
+    CONSTRAINT obra_idmaterial_fk FOREIGN KEY (idmaterial) REFERENCES Material(idmaterial),
+    CONSTRAINT obra_idestil_fk FOREIGN KEY (idestil) REFERENCES Estil(idestil)
+);
+
+CREATE TABLE autorxobra (
+    idobra SMALLINT,
+    idautor SMALLINT,
+    CONSTRAINT autorxobra PRIMARY KEY (idobra,idautor),
+    CONSTRAINT autorxobra FOREIGN KEY (idobra) REFERENCES obra(idobra),
+    CONSTRAINT autorxobra FOREIGN KEY (idautor) REFERENCES autor(idautor)
+);
+
+CREATE TABLE expoxmuseu (
+    idobra SMALLINT,
+    idmuseu SMALLINT,
+    
 );

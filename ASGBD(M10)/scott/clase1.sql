69f3c37e5c828439783a675f8cba129b01a19a27
gxldnn <riberajan@gmail.com>
2025-01-28 13:52:58 +0100

    Docker Push

--- ASGBD(M10)/scott/clase1.sql
+++ ASGBD(M10)/scott/clase1.sql
@@ -1,4 +1,4 @@
 SELECT ename FROM emp WHERE LOWER(ename) LIKE '_l%';
 
 
-SELECT ename,sal, COcomm, (sal+COALESCE(comm,0)) FROM emp;
+SELECT ename,sal, COALESCEcomm, (sal+COALESCE(comm,0)) FROM emp;

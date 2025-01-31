de493cc49fd482a7f8b5c03f02d0962b0d5faeb8
gxldnn <riberajan@gmail.com>
2025-01-31 11:28:03 +0100

    Docker Push

--- ASGBD(M10)/scott/clase2.sql
+++ ASGBD(M10)/scott/clase2.sql
@@ -2,4 +2,4 @@ SELECT ename,job, sal FROM emp WHERE ( job='SALESMAN' OR  job='PRESIDENT') AND s
 SELECT ename, job, deptno, hiredate FROM emp ORDER BY hiredate DESC;
 SELECT ename, job, deptno, hiredate FROM emp ORDER BY deptno, ename DESC;
 SELECT empno, ename, sal*12 AS "Salari Anual" FROM emp ORDER BY 3;
--- Mostrar nom del empleat el salari
+-- Mostrar nom del empleat el salari, el salari incre

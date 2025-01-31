2966962037d433457d2a4e02ea6ecddbe3911b2d
gxldnn <riberajan@gmail.com>
2025-01-31 11:24:59 +0100

    Docker Push

--- ASGBD(M10)/scott/clase2.sql
+++ ASGBD(M10)/scott/clase2.sql
@@ -1,4 +1,5 @@
 SELECT ename,job, sal FROM emp WHERE ( job='SALESMAN' OR  job='PRESIDENT') AND sal>1500; LOS PARENTESIS HACEN QUE SE PRIORIZE ESO --)
 SELECT ename, job, deptno, hiredate FROM emp ORDER BY hiredate DESC; -- DESC (Muestre valores de mayor a menor ) O ASC (Es lo que hace por defecto que es de menor a mayor)--)
 SELECT ename, job, deptno, hiredate FROM emp ORDER BY deptno, ename DESC;
--Hay que pones ASC o DESC al lado del campo sino no lo coje en este caso ordenara los nombres de ultimas letras del abecedario --hasta las del principio del abecedario de ename solamente --)SELECT empno, ename, sal*12 AS annsal FROM emp ORDER BY AS annsal; -- annsal son una especie de etiqueta sera el nombre del campo que aparezca
+-Hay que pones ASC o DESC al lado del campo sino no lo coje en este caso ordenara los nombres de ultimas letras del abecedario --hasta las del principio del abecedario de ename solamente --)
+SELECT empno, ename, sal*12 AS annsal FROM emp ORDER BY AS annsal; -- annsal son una especie de etiqueta sera el nombre del campo que aparezca

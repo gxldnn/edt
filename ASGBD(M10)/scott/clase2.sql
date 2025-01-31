bf2f101c7472078c0dcb3ca8d86c9e633543926e
gxldnn <riberajan@gmail.com>
2025-01-31 11:31:25 +0100

    Docker Push

--- ASGBD(M10)/scott/clase2.sql
+++ ASGBD(M10)/scott/clase2.sql
@@ -3,4 +3,4 @@ SELECT ename, job, deptno, hiredate FROM emp ORDER BY hiredate DESC;
 SELECT ename, job, deptno, hiredate FROM emp ORDER BY deptno, ename DESC;
 SELECT empno, ename, sal*12 AS "Salari Anual" FROM emp ORDER BY 3;
 -- Mostrar nom del empleat el salari, el salari incrementat 20% , de qui mes cobra a menys
-SELECT ename, sal, sal*1.20 AS "Salari Incrementat" FROM emp ORDER BY 3 DESC AND ORDER B
+SELECT ename, sal, sal*1.20 AS "Salari Incrementat" FROM emp ORDER BY 3 DESC AND ORDER BY ename

4428f3b87056211418bd538432bd86b182194055
gxldnn <138344266+gxldnn@users.noreply.github.com>
2025-02-03 09:49:17 +0100

    Update index.php

--- IAW(M09)/index.php
+++ IAW(M09)/index.php
@@ -4,26 +4,9 @@
     <meta charset="utf-8"/>
     <title>Pàgina de Jan Ribera</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="estils/estil.css"> <!-- atenció, cal crear-lo! -->
+    <link rel="stylesheet" href="styles.css">
   </head>
-  <style>
-    @import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
-    body {
-      background-image: url(https://media.tenor.com/pBR6qPBzNUUAAAAM/suit-nle-choppa.gif);
-      background-size:500px;
 
-    }
-    table {
-      border-collapse: collapse;
-      width: 50%;
-      margin: 20px auto;
-    }
-    th, td {
-      border: 1px solid black;
-      text-align: center;
-      padding: 10px;
-    }
-    </style>
   <body>
 
     
@@ -31,17 +14,24 @@
     <h1>Taula de Multiplicar</h1>
 
     <table>
+    <colgroup>
+      <col span="2">
+      <col span = "1" style="background-color:yellow">
+    </colgroup>
     <?php
-
       echo "<tr>";
-      echo "<th>X</th>";
       for ($i = 1; $i <= 10; $i++){
-      echo "<th>$i</th>";
+        if ($i == 1){
+          echo "<th>X</th>";
+        }
+        echo "<th>$i</th>";
       }
+      echo "</tr>";
       for ($y = 1 ;$y <= 10; $y++) {
         echo "<tr>";
         echo "<th>$y</th>";
         for ($x = 1; $x <=10; $x++){
+
           echo "<td>". $y * $x ."</td>";
         }
         echo "</tr>";

18115acdab004dc118f559d13090f92096023cd8
gxldnn <riberajan@gmail.com>
2025-02-04 13:13:52 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbd.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbd.sql
@@ -26,4 +26,4 @@
 --SELECT * FROM repventa WHERE fcontrato + 5000 BETWEEN '2003-01-01' AND '2003-12-31';º
 
 --10. 
---SELECT nombre, CURRENT_DATE-fcontrato FROM repventa;zsscoiahdhaso
+--SELECT nombre, CURRENT_DATE-fcontrato FROM repventa;zs

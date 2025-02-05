fdbe7ac8c82c5500be0ccba3a134997236cd33ad
gxldnn <riberajan@gmail.com>
2025-02-05 13:49:00 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -8,4 +8,5 @@
 --SELECT nombre, repcod, repventa.ofinum, ciudad, region FROM repventa JOIN oficina ON repventa.ofinum = oficina.ofinum ORDER BY 1;
 
 --4. 
-SELECT ciudad, region, repventa.nombre, objetivo FROM oficina JOIN repventa ON oficina.ofinum = repventa.ofinum WHERE objetivo > 360000;
+SELECT ciudad, region, repventa.nombre, objetivo 
+FROM oficina JOIN repventa ON oficina.ofinum = repventa.ofinum WHERE objetivo > 360000;

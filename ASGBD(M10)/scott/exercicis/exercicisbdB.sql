14fb36eefb7b6af2ce465ad53391eeb8e2445b8e
gxldnn <riberajan@gmail.com>
2025-02-05 13:38:47 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -1,5 +1,5 @@
 --1.
-SELECT nombre, ciudad, region FROM repventa JOIN oficina ON repventa.ofinum = oficina.ofinum
+--SELECT nombre, ciudad, region FROM repventa JOIN oficina ON repventa.ofinum = oficina.ofinum
 
 --2. 
 SELECT *, nombre FROM pedido JOIN cliente ON pedido.cliecod = cliente.cliecod

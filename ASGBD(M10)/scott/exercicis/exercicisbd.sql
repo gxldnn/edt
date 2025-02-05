d10b55022e1210224f9ecb81a03fbc8c7178ca94
gxldnn <riberajan@gmail.com>
2025-02-05 12:17:36 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbd.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbd.sql
@@ -1,29 +1,29 @@
 --1. 
---SELECT * FROM producto WHERE exist BETWEEN 25 AND 40;
+SELECT * FROM producto WHERE exist BETWEEN 25 AND 40;
 
 --2.
---SELECT repcod FROM repventa ORDER BY 1;
+SELECT DISTINCT repcod FROM pedido;
 
 --3.
---SELECT * FROM pedido WHERE cliecod = 2111;
+SELECT * FROM pedido WHERE cliecod = 2111;
 
 --4.
---SELECT * FROM pedido WHERE cliecod = 2111 AND repcod = 103;
+SELECT * FROM pedido WHERE cliecod = 2111 AND repcod = 103;
 
 --5.
---SELECT * FROM pedido WHERE cliecod = 2111 AND repcod = 103 AND LOWER(fabcod) = 'aci';
+SELECT * FROM pedido WHERE cliecod = 2111 AND repcod = 103 AND LOWER(fabcod) = 'aci';
 
 --6. 
---SELECT * FROM pedido ORDER BY cliecod, fecha;
+SELECT * FROM pedido ORDER BY cliecod, fecha;
 
 --7. 
---SELECT * FROM repventa WHERE ofinum IN (12, 13);
+SELECT * FROM repventa WHERE ofinum IN (12, 13);
 
 --8.
---SELECT * FROM producto WHERE exist IN (0, NULL); --per en el cas de que fos null, no faria falta
+SELECT * FROM producto WHERE exist IN (0, NULL); --per en el cas de que fos null, no faria falta
 
 --9.
---SELECT * FROM repventa WHERE fcontrato + 5000 BETWEEN '2003-01-01' AND '2003-12-31';º
+SELECT * FROM repventa WHERE fcontrato + 5000 BETWEEN '2003-01-01' AND '2003-12-31';
 
 --10. 
---SELECT nombre, CURRENT_DATE-fcontrato FROM repventa;
+SELECT nombre, CURRENT_DATE-fcontrato FROM repventa;

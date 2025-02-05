ef13a93eb65cc31a5910cb2b3df1abeec9c62131
gxldnn <riberajan@gmail.com>
2025-02-05 12:24:02 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbd.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbd.sql
@@ -11,13 +11,13 @@
 --SELECT * FROM pedido WHERE cliecod = 2111 AND repcod = 103;
 
 --5.
--SELECT * FROM pedido WHERE cliecod = 2111 AND repcod = 103 AND LOWER(fabcod) = 'aci';
+--SELECT * FROM pedido WHERE cliecod = 2111 AND repcod = 103 AND LOWER(fabcod) = 'aci';
 
 --6. 
-SELECT * FROM pedido ORDER BY cliecod, fecha;
+--SELECT * FROM pedido ORDER BY cliecod, fecha;
 
 --7. 
-SELECT * FROM repventa WHERE ofinum IN (12, 13);
+--SELECT * FROM repventa WHERE ofinum IN (12, 13);
 
 --8.
 SELECT * FROM producto WHERE exist IN (0, NULL); --per en el cas de que fos null, no faria falta

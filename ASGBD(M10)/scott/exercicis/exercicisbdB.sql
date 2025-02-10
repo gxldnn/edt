c836c94bdc17f4465376b9714d1c8c4ad5077a7c
gxldnn <riberajan@gmail.com>
2025-02-10 12:49:11 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -10,7 +10,26 @@
 
 
 --4. 
+/*
+SELECT ciudad, oficina.director, repventa.puesto, objetivo 
+FROM oficina JOIN repventa 
+ON repventa.ofinum = oficina.ofinum 
+WHERE oficina.objetivo > 360000*/
 
-SELECT ciudad, region, repventa.jefe, objetivo 
-FROM repventa JOIN oficina ON repventa.ofinum = oficina.ofinum;
- 
+--5.
+/*
+SELECT pednum, importe, producto.descrip
+FROM pedido JOIN producto 
+ON producto.prodcod = pedido.prodcod;
+*/
+--6. 
+/*
+SELECT pednum, importe, cliente.nombre "Nom Client", producto.descrip, repventa.nombre "Nom Representant"
+FROM pedido JOIN cliente
+ON cliente.cliecod = pedido.cliecod
+JOIN producto
+ON cliente.cliecod = pedido.cliecod
+JOIN repventa
+ON repventa.repcod = pedido.repcod
+WHERE importe > 400 ORDER BY 3, 2 DESC;
+*/

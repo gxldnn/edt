903e1f362dae74b5f031bb4efe42412ca7343f6f
gxldnn <riberajan@gmail.com>
2025-02-11 11:41:21 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -55,5 +55,5 @@ FROM pedido JOIN cliente
 ON cliente.cliecod =  pedido.cliecod
 JOIN repventa
 ON repventa.repcod = pedido.repcod
-WHERE fecha = (SELECT MIN(fecha));
+WHERE fecha = (SELECT MIN(fecha) FROM );
 

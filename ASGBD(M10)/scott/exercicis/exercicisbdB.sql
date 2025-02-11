e17368789dad7b5d0512c6f73e78538f24028e33
gxldnn <riberajan@gmail.com>
2025-02-11 11:32:02 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -50,7 +50,7 @@ WHERE importe > 400 ORDER BY 3, 2 DESC;
 --7. Obtenir una llista de les comandes amb imports superiors  a 2000 euros, mostrant el número de comanda, import, nom del
 --client que ho va sol·licitar i el nom del representant que va contactar amb el client per primera vegada.
 
-SELECT pednum, importe, cliente.nombre, repventa
+SELECT pednum, importe, cliente.nombre, repventa.repcod
 FROM pedido JOIN cliente
 ON cliente.cliecod =  pedido.cliecod
 JOIN repventa

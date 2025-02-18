441651bbecd61d7b9c5307441af5fa4b0b5f3e50
gxldnn <riberajan@gmail.com>
2025-02-18 13:37:48 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -78,7 +78,7 @@ WHERE importe > 150 ORDER BY 3;
 SELECT p.pednum, p.importe, cliente.nombre, p.fecha, producto.descrip
 FROM pedido p
 JOIN cliente c  ON p.cliecod = cliente.cliecod
-JOIN producto pr ON pedido.fabcod = producto.fabcod AND pedido.prodcod = producto.prodcod
+JOIN producto pr ON pedido.fabcod = producto.fabcod AND p.prodcod = producto.prodcod
 WHERE pedido.fecha BETWEEN '2003-10-01' AND '2003-10-31';
 
 

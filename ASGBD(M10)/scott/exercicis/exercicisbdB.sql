c3ba9b9da9e3ad55bb40c478107a200cf41ae33d
gxldnn <riberajan@gmail.com>
2025-02-18 13:23:59 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -101,4 +101,4 @@ SELECT DISTINCT producto.descrip
 FROM pedido
 JOIN producto ON pedido.fabcod = producto.fabcod AND pedido.prodcod = producto.prodcod
 WHERE pedido.cant > producto.exist;
---
+--.

65ba26b8eba74db6105992353627cd9eda31adce
gxldnn <riberajan@gmail.com>
2025-02-18 13:32:53 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -69,7 +69,7 @@ SELECT p.pednum, p.importe, c.nombre, r.nombre, o.ciudad
 FROM pedido p 
 JOIN cliente c ON p.cliecod = c.cliecod
 JOIN repventa r ON r.repcod = c.repcod
-JOIN oficina o ON 
+JOIN oficina o ON r.
 WHERE importe > 150;
 
 --9. 

065267ee47594b739c01b6c059b7e458c9eff83d
gxldnn <riberajan@gmail.com>
2025-02-18 13:59:09 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -121,7 +121,8 @@ JOIN oficina o ON r.ofinum = o.ofinum;
 --14.
 SELECT pr.descrip 
 FROM producto pr
-JOIN pedido p ON p.fabcod = pr.fabcod AND p.prodcod = pr.prod
+JOIN pedido p ON p.fabcod = pr.fabcod AND p.prodcod = pr.prodcod
+
 
 --15.
 

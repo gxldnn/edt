7691742c11fa620b44aa08ae6a4f0b3cef358f2b
gxldnn <riberajan@gmail.com>
2025-02-18 13:53:40 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -103,10 +103,9 @@ WHERE r.fcontrato = p.fecha
 
 --12.
 
-SELECT repventa.nombre, repventa.cuota, oficina.ciudad, oficina.objetivo
+SELECT r.nombre, repventa.cuota, oficina.ciudad, oficina.objetivo
 FROM repventa
-JOIN oficina ON repventa.ofinum = oficina.ofinum
-WHERE repventa.cuota >= oficina.objetivo;
+
 
 /*
 --13. 

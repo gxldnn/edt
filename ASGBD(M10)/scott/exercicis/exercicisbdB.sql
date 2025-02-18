7376e45a7fd5496a64914bfbe080b0cddaf146ec
gxldnn <riberajan@gmail.com>
2025-02-18 13:48:12 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -90,7 +90,7 @@ FROM pedido p
 JOIN repventa r ON p.repcod = r.repcod
 JOIN oficina o ON r.ofinum = o.ofinum
 JOIN producto pr ON p.fabcod = pr.fabcod AND p.prodcod = pr.prodcod
-WHERE o.region = "ste";
+WHERE o.region = "Oste";
 
 --11.
 /*

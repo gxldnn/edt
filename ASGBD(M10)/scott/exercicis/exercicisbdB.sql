2404489ab1a0bd0d937f15d818dfbec6874b95f4
gxldnn <riberajan@gmail.com>
2025-02-18 13:42:26 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -87,7 +87,7 @@ WHERE p.fecha BETWEEN '2003-10-01' AND '2003-10-31';
 
 SELECT pednum
 FROM pedido p
-JOIN repventa r ON p.repcod = 
+JOIN repventa r ON p.repcod = r.rep
 JOIN oficina o ON r.repcod = o.repcod
 WHERE LOWER(o.region) = "este"
 

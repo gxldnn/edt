b1d61597ec4c3d75fb667542d73238ec1fddae4e
gxldnn <riberajan@gmail.com>
2025-02-18 09:39:04 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -67,6 +67,6 @@ AND fecha = (SELECT MIN(fecha) FROM pedido WHERE cliente.cliecod = pedido.clieco
 --representant treballa.
 
 SELECT pednum, importe, c.nombre FROM pedido p
-JOIN cliente c ON cliente.cliecod = pedido.cliecod
+JOIN cliente c ON cl.cliecod = pedido.cliecod
 
 WHERE importe > 150;

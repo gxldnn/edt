2083a04872d5ed29a326c2402e14b66c209569bd
gxldnn <riberajan@gmail.com>
2025-02-18 13:21:25 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -81,4 +81,9 @@ JOIN repventa ON pedido.repcod = repventa.repcod
 JOIN oficina ON repventa.ofinum = oficina.ofinum
 JOIN producto ON pedido.fabcod = producto.fabcod AND pedido.prodcod = producto.prodcod
 WHERE oficina.region = 'Est';
---1
+--11.
+SELECT pedido.pednum, pedido.importe, pedido.fecha
+FROM pedido
+JOIN repventa ON pedido.repcod = repventa.repcod
+WHERE pedido.fecha = repventa.fcontrato;
+

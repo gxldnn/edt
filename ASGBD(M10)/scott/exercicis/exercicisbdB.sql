a534ada0814d62bf006bc193db8d4df265aca329
gxldnn <riberajan@gmail.com>
2025-02-19 13:42:53 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -131,7 +131,7 @@ WHERE p.cant > pr.exist
 SELECT r.nombre
 FROM repventa r
 JOIN oficina o ON o.director = r.repcod
-JOIN repventa rdir ON rdir.repcod = o.directort
+JOIN repventa rdir ON rdir.repcod = o.director
 WHERE r.cuota > 
 
 

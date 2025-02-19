7c3a47093e44cfc594d93a10923a751c828ceba6
gxldnn <riberajan@gmail.com>
2025-02-19 13:57:08 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -132,5 +132,4 @@ SELECT *
 FROM repventa r
 JOIN oficina o ON o.ofinum = r.ofinum
 JOIN repventa rdir ON o.director = rdir.repcod
---JOIN repventa rdir ON r.repcod = rdir.repcod
---WHERE r.cuota > rdir.cuota
+-WHERE r.cuota > rdir.cuota

8d10da0b60f6afc7f6f98cd5ec391e9fa11f4084
gxldnn <riberajan@gmail.com>
2025-02-19 13:01:40 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -131,7 +131,7 @@ WHERE p.cant > pr.exist
 --15.
 SELECT e.ename"Nom", s.grade"Grau"
 FROM emp
-WHERE e.sal >= s.losal AND e.sal <= s.hisae
+WHERE e.sal >= s.losal AND e.sal <= s.hisal;
 
 --16.
 /*

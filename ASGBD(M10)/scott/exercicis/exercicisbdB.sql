193f5d7a13daae6662fc7c47c91d3aa73e6fa5ab
gxldnn <riberajan@gmail.com>
2025-02-26 13:06:11 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -123,7 +123,7 @@ LEFT JOIN oficina o ON r.ofinum = o.ofinum;
 
 SELECT pr.descrip 
 FROM producto pr
-JOIN pedido p ON p.fabcod||p.prodcod = pr.prodcod||pr.p 
+JOIN pedido p ON p.fabcod||p.prodcod = pr.prodcod||pr.prodcos 
 WHERE p.cant > pr.exist
 
 

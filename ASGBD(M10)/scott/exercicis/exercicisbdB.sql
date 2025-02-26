b2e93f5a09a9b92e32fcdd962c8bf641859242ef
gxldnn <riberajan@gmail.com>
2025-02-26 13:49:21 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -145,10 +145,6 @@ WHERE r.ofinum != rdir.ofinum
 
 */
 
-SELECT r.nombre, r.ofinum, rdir.nombre, rdir.ofinum
-FROM repventa r
-JOIN repventa rdir ON r.jefe = rdir.repcod
-WHERE r.ofinum != rdir.ofinum
 
 
 

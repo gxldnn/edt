e1616ea68bfdeace14d85a13297c82f692556f49
gxldnn <riberajan@gmail.com>
2025-02-28 11:39:36 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -168,6 +168,7 @@ SELECT r.nombre "Nom Repv", COALESCE(o.ciudad,'No te Ciutat') "Ciutat", p.pednum
 FROM repventa r
 JOIN producto pr ON p.fabcod||p.prodcod = pr.fabcod||pr.prodcod
 JOIN cliente c ON p.cliecod = c.cliecod
+LEFT JOIN oficina o ON r.ofinum = o.ofinum
 LEFT JOIN pedido p ON r.repcod = p.repcod
 ORDER BY 1;
 

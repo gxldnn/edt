04a9990544bfc05c86464df80b458642279905fc
gxldnn <riberajan@gmail.com>
2025-02-28 11:39:33 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -166,7 +166,6 @@ JOIN repventa rdir ON r.repcod = rdir.nombre
 --Per a cada representant mostrar el nom i le seva oficina, numero de comanda i l'import
 SELECT r.nombre "Nom Repv", COALESCE(o.ciudad,'No te Ciutat') "Ciutat", p.pednum"Numero de comanda", p.importe "Import" , pr.descrip "Producto", c.nombre "Nom del Client"
 FROM repventa r
-LEFT JOIN oficina o ON r.ofinum = o.ofinum
 JOIN producto pr ON p.fabcod||p.prodcod = pr.fabcod||pr.prodcod
 JOIN cliente c ON p.cliecod = c.cliecod
 LEFT JOIN pedido p ON r.repcod = p.repcod

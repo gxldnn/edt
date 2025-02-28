a22d79c4989d732206e51ce5f231f4a7841cab95
gxldnn <riberajan@gmail.com>
2025-02-28 10:40:44 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdB.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdB.sql
@@ -169,7 +169,7 @@ FROM repventa r
 LEFT JOIN oficina o ON r.ofinum = o.ofinum
 JOIN pedido p ON r.repcod = p.repcod
 JOIN producto pr ON p.fabcod||p.prodcod = pr.fabcod||pr.prodcod
-JOIN 
+JOIN clie
 ORDER BY 1;
 
 --Numero de comanda i l'import

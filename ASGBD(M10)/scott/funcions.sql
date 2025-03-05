b3799331641ae8d353a9b1da50e1347b19018f99
gxldnn <riberajan@gmail.com>
2025-03-05 13:58:14 +0100

    Docker Push

--- ASGBD(M10)/scott/funcions.sql
+++ ASGBD(M10)/scott/funcions.sql
@@ -11,8 +11,14 @@ GROUP BY job;
 */
 
 -- 44. Seguint el que s'ha explicat en l'ex. 27, Mostreu per cada departament, el nombre d'empleats que tenen comissió, la suma i la mitjana.
---SELECT count(comm), sum(comm), ROUND(AVG(COALESCE(comm,0)),2) FROM emp;
+SELECT deptno, count(empno), sum(comm), ROUND(AVG(COALESCE(comm,0)),2)
+FROM emp
+WHERE comm IS NOT NULL
+GROUP BY deptno;
 
--- 45. Idem que el 4, però mostrant a més el nom de departament.
+
+-- 45. Idem que el 44, però mostrant a més el nom de departament.
+--SELECT d.dname, count(comm), sum(comm), ROUND(AVG(COALESCE(comm,0)),2) FROM emp e
+--JOIN dept d ON e.deptno = d.deptno;
 
 

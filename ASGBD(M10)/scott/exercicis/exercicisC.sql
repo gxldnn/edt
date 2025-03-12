1d60a8a2f3f5736f014611f4cb54631a413edf1f
gxldnn <riberajan@gmail.com>
2025-03-12 13:51:36 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisC.sql
+++ ASGBD(M10)/scott/exercicis/exercicisC.sql
@@ -5,5 +5,5 @@ SELECT SUM(cuota), SUM(ventas)
 FROM repventa;
 
 
---2\. Quin és l'import total de les comandes preses per Bill Adams?
+-- 2\. Quin és l'import total de les comandes preses per Bill Adams?
 

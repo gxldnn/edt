f2c4d3fa9bc80bf1a38e845f624fa4a5a12b1aad
gxldnn <riberajan@gmail.com>
2025-03-12 12:29:40 +0100

    Docker Push

--- ASGBD(M10)/scott/funcions.sql
+++ ASGBD(M10)/scott/funcions.sql
@@ -16,6 +16,7 @@ SELECT deptno, count(empno), sum(comm), ROUND(AVG(COALESCE(comm,0)),2)
 FROM emp
 WHERE comm IS NOT NULL
 GROUP BY deptno;
+
 SELECT deptno, COUNT (comm), SUM (comm), AVG (COALESCE(comm))
 FROM emp
 WHERE deptno IN (10,20)

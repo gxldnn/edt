9523d910a21f43661fdadd0664f33bea0f845cd7
gxldnn <riberajan@gmail.com>
2025-03-12 13:26:05 +0100

    Docker Push

--- ASGBD(M10)/scott/funcions.sql
+++ ASGBD(M10)/scott/funcions.sql
@@ -64,7 +64,7 @@ SELECT SAL FROM EMP WHERE SAL >=2000 AND SAL <=3000;
 --31. Mostreu el salari mínim, màxim i mig dels empleats agrupats per feina, però només d'aquells la mitjana sigui superior a 4000.
 
 
-select min(sal) 
+select min(sal) and 
 
 
 

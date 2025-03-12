aa1130d08f5e15e38831a080258940b7e947c11e
gxldnn <riberajan@gmail.com>
2025-03-12 14:02:07 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisC.sql
+++ ASGBD(M10)/scott/exercicis/exercicisC.sql
@@ -26,5 +26,5 @@ WHERE LOWER(fabcod) = 'aci';
 SELECT AVG(pr.precio)
 FROM pedido p
 JOIN producto pr ON pr.fabcod||pr.prodcod = p.fabcod||p.prodcod
-JOIN clien
+JOIN cliente c ON 
 WHERE LOWER(p.cliecod) = 'acme mfg'

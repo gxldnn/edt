aa2596af6fcf75ac427866f948607ac432f163cd
gxldnn <riberajan@gmail.com>
2025-03-12 13:52:55 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisC.sql
+++ ASGBD(M10)/scott/exercicis/exercicisC.sql
@@ -1,10 +1,10 @@
 -- 1\. Mostrar la suma de les quotes i la suma de les vendes totals de
 -- tots els representants.
-
+/*
 SELECT SUM(cuota), SUM(ventas)
 FROM repventa;
 
-
+*/
 -- 2\. Quin és l'import total de les comandes preses per Bill Adams?
 
 SELECT ventas 

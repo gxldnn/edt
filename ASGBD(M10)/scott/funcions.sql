ad66737b362836718bd2087c9a7a20fbb16c40b4
gxldnn <riberajan@gmail.com>
2025-03-12 12:26:01 +0100

    Docker Push

--- ASGBD(M10)/scott/funcions.sql
+++ ASGBD(M10)/scott/funcions.sql
@@ -11,6 +11,7 @@ GROUP BY job;
 */
 
 -- 44. Seguint el que s'ha explicat en l'ex. 27, Mostreu per cada departament, el nombre d'empleats que tenen comissió, la suma i la mitjana.
+/*
 SELECT deptno, count(empno), sum(comm), ROUND(AVG(COALESCE(comm,0)),2)
 FROM emp
 WHERE comm IS NOT NULL

6bfe5ac29aca3f6c23552498064d996433e7cb72
gxldnn <riberajan@gmail.com>
2025-03-14 12:06:32 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisC.sql
+++ ASGBD(M10)/scott/exercicis/exercicisC.sql
@@ -137,4 +137,4 @@ SELECT SUM(p.importe), c.cliecod, r.repcod
 FROM pedido p 
 JOIN repventa r ON p.repcod = r.repcod
 JOIN cliente c ON r.repcod = c.repcod
-GROUP BY c.cliecod r.repcod;
+GROUP BY c.cliecod, r.repcod;

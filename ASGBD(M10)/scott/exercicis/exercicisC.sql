ad76d386881f8f31d7dcd8a66c5a0cb3cad798cd
gxldnn <riberajan@gmail.com>
2025-03-14 11:35:41 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisC.sql
+++ ASGBD(M10)/scott/exercicis/exercicisC.sql
@@ -88,6 +88,6 @@ FROM repventa;
 --13\. Quantes oficines tenen representants que superen les seves
 --pròpies quotes?
 
-SELECT count(DISTINCT ofinum)
+SELECT DISTINCT ofinum)
 FROM repventa
 WHERE ventas > cuota;

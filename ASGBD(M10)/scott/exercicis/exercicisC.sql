951c9ca89b47692794087a7db0d60e0f4e5a2ec3
gxldnn <riberajan@gmail.com>
2025-03-14 11:47:30 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisC.sql
+++ ASGBD(M10)/scott/exercicis/exercicisC.sql
@@ -117,5 +117,5 @@ ORDER BY ofinum;
 
 SELECT count(r.repcod)"Nombre representants", o.ciudad "Ciutat"
 FROM repventa r
-JOIN oficina o ON r.ofinum = o.ofinum;
+JOIN oficina o ON r.ofinum = o.ofinum
 GROUP BY o.ofinum;

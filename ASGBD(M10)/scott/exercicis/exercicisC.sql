fff0877dc436c2c62eb736d71c7c8ef76848d364
gxldnn <riberajan@gmail.com>
2025-03-14 12:14:08 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisC.sql
+++ ASGBD(M10)/scott/exercicis/exercicisC.sql
@@ -156,4 +156,5 @@ ORDER BY r.repcod;
 --20\. Per a cada oficina amb dos o més representants, calculeu el total de
 --les quotes i el total de les vendes de tots els representants.
 
-SELECT count(r.repcod), ofinum, 
+SELECT count(r.repcod), ofinum
+F 

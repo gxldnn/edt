34126207efd9e2acbb39b1b9b765c9939cc48620
gxldnn <riberajan@gmail.com>
2025-03-14 10:45:14 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisC.sql
+++ ASGBD(M10)/scott/exercicis/exercicisC.sql
@@ -60,6 +60,6 @@ FROM cliente;
 --9\. Quants representants han obtingut un import de vendes superior a
 --la seva pròpia quota?
 
-SELECT count()
+SELECT count(recpod)
 FROM repventa
 WHERE ventas > cuota;

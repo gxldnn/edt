c5423f4a7e1eecefcb17ed22cf53f4a5729b9116
gxldnn <riberajan@gmail.com>
2025-03-18 12:19:42 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisC.sql
+++ ASGBD(M10)/scott/exercicis/exercicisC.sql
@@ -126,9 +126,8 @@ JOIN cliente c ON r.repcod = c.repcod
 GROUP BY r.repcod;
 */
 
---18\. Calcula el total de l'import de les comandes sol·licitades per cada
+--8\. Calcula el total de l'import de les comandes sol·licitades per cada
 --client a cada representant.
-
 /*
 SELECT SUM(p.importe), c.cliecod, r.repcod
 FROM pedido p 

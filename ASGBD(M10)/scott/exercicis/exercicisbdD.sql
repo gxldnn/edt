b075e41806e2e97414407ef3f2d1131fbc4a9797
gxldnn <riberajan@gmail.com>
2025-03-19 12:59:50 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdD.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdD.sql
@@ -64,7 +64,8 @@ WHERE fabcod||prodcod IN (SELECT fabcod||prodcod FROM pedido WHERE importe > 150
 SELECT *
 FROM cliente 
 WHERE repcod IN 
-    (SELECT repcod FROM repventa WHERE nombre = 'Sue Smith') AND cliecod IN (SELECT cliecod FROM pedido WHERE importe < 5000)
+    (SELECT repcod FROM repventa WHERE nombre = 'Sue Smith') AND 
+    cliecod IN (SELECT cliecod FROM pedido WHERE importe < 5000)
 */
 
 --9\. Obtenir una llista de les oficines on hi hagi algun representant

6639499aea58f6fed60fbe10e8ef24a27cb1ff3b
gxldnn <riberajan@gmail.com>
2025-03-19 13:01:02 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdD.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdD.sql
@@ -76,7 +76,7 @@ WHERE repcod IN
 SELECT *
 FROM oficina
 WHERE ofinum IN 
-    (SELECT ofinum FROM repventa WHERE cuo)
+    (SELECT ofinum FROM repventa WHERE cuota )
 
 --10\. Obtenir una llista dels representants que han pres alguna comanda
 --l'import del qual sigui més del 10% de la seva quota.

3aa126be93a4e81265a272b8979d859e5178fb36
gxldnn <riberajan@gmail.com>
2025-03-19 12:16:59 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdD.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdD.sql
@@ -47,7 +47,7 @@ WHERE ofinum != ALL (SELECT o.ofinum FROM oficina o JOIN repventa r ON r.repcod 
 SELECT * 
 FROM cliente
 WHERE  
-*/
+
 --7\. Obtenir una llista dels productes dels quals s'ha pres una comanda
 --de 150 euros o més.
 

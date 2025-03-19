417ee4c7c3b2f4b0a26fa3f009bd1e0ad5bd7474
gxldnn <riberajan@gmail.com>
2025-03-19 13:42:22 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdD.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdD.sql
@@ -83,7 +83,7 @@ WHERE ofinum IN
 --l'import del qual sigui més del 10% de la seva quota.
 
 SELECT *
-FROM repventa
+FROM repventa 
 WHERE cuota > ANY (SELECT importe FROM pedido p JOIN repventa r ON p.repcod = r.repcod);
 
 

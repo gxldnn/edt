a26baebf9dd4c8f65032a38aae205f9bbdf2d2dd
gxldnn <riberajan@gmail.com>
2025-03-19 12:43:37 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdD.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdD.sql
@@ -61,6 +61,8 @@ WHERE fabcod||prodcod IN (SELECT fabcod||prodcod FROM pedido WHERE importe > 150
 --8\. Obtenir una llista dels clients contactats per Sue Smith que no
 --han sol·licitat comandes amb imports superiors a 18 euros.
 
+
+
 --9\. Obtenir una llista de les oficines on hi hagi algun representant
 --la quota del qual sigui més del 55% de l'objectiu de l'oficina. Per comprovar el vostre
 --exercici, feu una Consulta prèvia el resultat de la qual valideu l'exercici.

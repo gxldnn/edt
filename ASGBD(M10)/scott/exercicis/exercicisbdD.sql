0c33b0176eb658f1d75abf94dc9c5156ade3f059
gxldnn <riberajan@gmail.com>
2025-03-19 13:46:21 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdD.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdD.sql
@@ -81,12 +81,12 @@ WHERE ofinum IN
 
 --10\. Obtenir una llista dels representants que han pres alguna comanda
 --l'import del qual sigui més del 10% de la seva quota.
-
+/* NO ESTA 
 SELECT *
 FROM repventa r
 WHERE 0.1*cuota > ANY (SELECT importe FROM pedido p WHERE p.repcod = r.repcod);
 
-
+*/
 --11\. Obtenir una llista de les oficines on el total de vendes
 --dels seus representants han aconseguit un import de vendes que supera el
 --50% de lobjectiu de loficina. Mostrar també l'objectiu de cada

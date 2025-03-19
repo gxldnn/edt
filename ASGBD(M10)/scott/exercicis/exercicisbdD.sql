bd409bc0f0fea4e74524ae40392b4ab8ffa59b46
gxldnn <riberajan@gmail.com>
2025-03-19 12:55:42 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdD.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdD.sql
@@ -63,7 +63,7 @@ WHERE fabcod||prodcod IN (SELECT fabcod||prodcod FROM pedido WHERE importe > 150
 
 SELECT *
 FROM cliente 
-WHERE cliecod = (SELECT cliecod FROM pedido WHERE importe < 18)
+WHERE cliecod = (SELECT cliecod FROM pedido WHERE importe < 300)
 
 --9\. Obtenir una llista de les oficines on hi hagi algun representant
 --la quota del qual sigui més del 55% de l'objectiu de l'oficina. Per comprovar el vostre

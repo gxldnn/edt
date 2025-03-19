7cdb3676053fb60eaa12d9adbcf000cd0d664f54
gxldnn <riberajan@gmail.com>
2025-03-19 13:11:16 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdD.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdD.sql
@@ -84,7 +84,7 @@ WHERE ofinum IN
 
 SELECT *
 FROM repventa
-WHERE repcod = (SLEE)
+WHERE repcod = ()
 
 --11\. Obtenir una llista de les oficines on el total de vendes
 --dels seus representants han aconseguit un import de vendes que supera el

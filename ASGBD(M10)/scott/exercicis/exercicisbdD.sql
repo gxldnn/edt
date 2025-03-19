9b01de9c1846776b36eb15d2fab8f17900c7b158
gxldnn <riberajan@gmail.com>
2025-03-19 12:41:44 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdD.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdD.sql
@@ -56,7 +56,7 @@ WHERE cliecod = (SELECT cliecod FROM pedido WHERE fabcod = 'aci' AND fecha BETWE
 
 SELECT *
 FROM prodcuto
-WHERE fabcod||prodcod
+WHERE fabcod||prodcod IN ()
 
 --8\. Obtenir una llista dels clients contactats per Sue Smith que no
 --han sol·licitat comandes amb imports superiors a 18 euros.

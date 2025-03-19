ff3c84004a9b680d7a5628d0bef4112b45e541e8
gxldnn <riberajan@gmail.com>
2025-03-19 12:40:45 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/exercicisbdD.sql
+++ ASGBD(M10)/scott/exercicis/exercicisbdD.sql
@@ -46,14 +46,15 @@ WHERE ofinum != ALL (SELECT o.ofinum FROM oficina o JOIN repventa r ON r.repcod 
 
 
 --INSERT INTO pedido VALUES (113015, '2003-01-11', 2111, 105, 'aci', 41003, 35, 3745.00); INSERT PER A QUE HI HAGI UN VALOR QUE BUSCAR
-
+/*
 SELECT * 
 FROM cliente
 WHERE cliecod = (SELECT cliecod FROM pedido WHERE fabcod = 'aci' AND fecha BETWEEN '2003-01-01' AND '2003-06-30');
-
+*/
 --7\. Obtenir una llista dels productes dels quals s'ha pres una comanda
 --de 150 euros o més.
 
+SE
 
 --8\. Obtenir una llista dels clients contactats per Sue Smith que no
 --han sol·licitat comandes amb imports superiors a 18 euros.

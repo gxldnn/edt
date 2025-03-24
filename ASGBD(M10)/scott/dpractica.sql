92c98c2fab873811479f8e2e0dfecc1214865b07
gxldnn <riberajan@gmail.com>
2025-03-24 09:16:37 +0100

    Docker Push

--- ASGBD(M10)/scott/dpractica.sql
+++ ASGBD(M10)/scott/dpractica.sql
@@ -48,7 +48,7 @@ WHERE ofinum NOT IN (SELECT ofinum FROM repventa WHERE nombre = 'Larry Fitch');
 
 SELECT *
 FROM cliente
-WHERE ped
+WHERE pednum
 
 --7\. Obtenir una llista dels productes dels quals s'ha pres una comanda
 --de 150 euros o més.

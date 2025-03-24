8976412802b0243b8b132870cef85766a6831d75
gxldnn <riberajan@gmail.com>
2025-03-24 09:04:09 +0100

    Docker Push

--- ASGBD(M10)/scott/dpractica.sql
+++ ASGBD(M10)/scott/dpractica.sql
@@ -9,7 +9,7 @@ ORDER BY 1;
 
 SELECT *
 FROM repventa 
-WHERE cuota > ANY (SELECT objectivo FROM oficina WHERE ciudad)
+WHERE cuota > ANY (SELECT objectivo FROM oficina WHERE LOWER(ciudad)
 
 --2\. Obtenir una llista de tots els clients (nom) que van ser
 --contactats per primera vegada per Bill Adams.

6abe248738b69161cd6b50a74150535d6319d5a6
gxldnn <riberajan@gmail.com>
2025-03-24 09:12:01 +0100

    Docker Push

--- ASGBD(M10)/scott/dpractica.sql
+++ ASGBD(M10)/scott/dpractica.sql
@@ -34,7 +34,7 @@ WHERE fabcod = 'aci' AND exist  > ANY (SELECT exist FROM producto WHERE prodcod 
 
 SELECT *
 FROM repventa r
-WHERE ventas > (SELECT objetivo FROM oficina)
+WHERE ventas > (SELECT objetivo FROM oficina WHERE )
 
 --5\. Obtenir una llista dels representants que no treballen a les
 --oficines dirigides per Larry Fitch.

611ad769ae5ad096e3e3228b397adc4e6c1fcf6a
gxldnn <riberajan@gmail.com>
2025-03-24 09:13:43 +0100

    Docker Push

--- ASGBD(M10)/scott/dpractica.sql
+++ ASGBD(M10)/scott/dpractica.sql
@@ -40,7 +40,8 @@ WHERE ventas > (SELECT objetivo FROM oficina o WHERE r.ofinum = o.ofinum);
 --oficines dirigides per Larry Fitch.
 
 SELECT * 
-FROM reo
+FROM repventa 
+
 
 --6\. Obtenir una llista de tots els clients que han demanat comandes
 --del fabricant ACI entre gener i juny del 2003.

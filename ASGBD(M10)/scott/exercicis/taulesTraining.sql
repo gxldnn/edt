d643192cecefc846377fce9724f3dd2deaf34c0f
gxldnn <riberajan@gmail.com>
2025-03-24 09:18:40 +0100

    Docker Push

--- ASGBD(M10)/scott/exercicis/taulesTraining.sql
+++ ASGBD(M10)/scott/exercicis/taulesTraining.sql
@@ -140,7 +140,7 @@ COPY repventa (repcod, nombre, edad, ofinum, puesto, fcontrato, jefe, cuota, ven
 106	Sam Clark	52	11	VP Ventas	1988-06-14	\N	275000.00	299912.00
 104	Bob Smith	33	12	Dir Ventas	1987-05-19	106	200000.00	142594.00
 101	Dan Roberts	45	12	Representante Ventas	1986-10-20	104	300000.00	305673.00
-110	Tom Snyder	41	\N	Representante Ventas	2003-01-13	101	\N	75985.00
+110	Tom Snyder	41	\N	Representante Ventas	1990-01-13	101	\N	75985.00
 108	Larry Fitch	62	21	Dir Ventas	1989-10-12	106	350000.00	361865.00
 103	Paul Cruz	29	12	Representante Ventas	1987-03-01	104	275000.00	286775.00
 107	Nancy Angelli	49	22	Representante Ventas	1988-11-14	108	300000.00	186042.00

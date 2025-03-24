20ba3290200eaa10930ca99b604bfa414fb7bbac
gxldnn <riberajan@gmail.com>
2025-03-24 09:01:45 +0100

    Docker Push

--- ASGBD(M10)/scott/dpractica.sql
+++ ASGBD(M10)/scott/dpractica.sql
@@ -1,7 +1,7 @@
 --0. Mostrar el nom i el lloc dels que són cap (ja està fet amb self join, ara amb subconsultes)
 
 SELECT repcod, puesto
-FRO
+FROM repventa 
 --1\. Obtenir una llista dels representants les quotes dels quals són iguals o
 --superiors a lobjectiu de loficina dAtlanta.
 --

5636334aed99da58b121a855b0a7451bd9aff916
gxldnn <riberajan@gmail.com>
2025-03-24 09:02:02 +0100

    Docker Push

--- ASGBD(M10)/scott/dpractica.sql
+++ ASGBD(M10)/scott/dpractica.sql
@@ -2,7 +2,7 @@
 
 SELECT repcod, puesto
 FROM repventa
-WHERE repcod = 
+WHERE repcod = jefe
 --1\. Obtenir una llista dels representants les quotes dels quals són iguals o
 --superiors a lobjectiu de loficina dAtlanta.
 --

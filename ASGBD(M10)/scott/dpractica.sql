f758d1d60567936e7f42b8db678ff8d15f6711e8
gxldnn <riberajan@gmail.com>
2025-03-24 09:13:45 +0100

    Docker Push

--- ASGBD(M10)/scott/dpractica.sql
+++ ASGBD(M10)/scott/dpractica.sql
@@ -41,7 +41,7 @@ WHERE ventas > (SELECT objetivo FROM oficina o WHERE r.ofinum = o.ofinum);
 
 SELECT * 
 FROM repventa 
-
+WHERE ofinum 
 
 --6\. Obtenir una llista de tots els clients que han demanat comandes
 --del fabricant ACI entre gener i juny del 2003.

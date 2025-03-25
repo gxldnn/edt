0898f664f59bb02b5f93bdd4cf669638b26d8b89
gxldnn <riberajan@gmail.com>
2025-03-25 10:20:04 +0100

    Docker Push

--- ASGBD(M10)/training/d.sql
+++ ASGBD(M10)/training/d.sql
@@ -107,7 +107,7 @@ WHERE prodcod||fabcod = (SELECT prodcod||fabcod FROM pedido WHERE fecha = (SELEC
 
 SELECT repcod, nombre
 FROM repventa
-WHERE ventas
+WHERE ventas 
 
 --14\. Quin representant té el pitjor percentatge de vendes?
 

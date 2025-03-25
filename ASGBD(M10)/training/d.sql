6dd546b7c7650c45abddcf2887adccac2b78fdef
gxldnn <riberajan@gmail.com>
2025-03-25 10:17:43 +0100

    Docker Push

--- ASGBD(M10)/training/d.sql
+++ ASGBD(M10)/training/d.sql
@@ -101,7 +101,7 @@ WHERE 0.5 * o.objetivo < ANY (SELECT SUM(ventas) FROM repventa r WHERE r.ofinum 
 
 SELECT descrip
 FROM producto
-WHERE prodcod||fabcod = (SELECT prodcod||fabcod FROM pedido WHERE fecha = MIN(fecha))
+WHERE prodcod||fabcod = (SELECT prodcod||fabcod FROM pedido WHERE fecha = ()))
 
 --13\. Quin representant té el millor percentatge de vendes?
 

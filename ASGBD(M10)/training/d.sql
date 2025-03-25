ad5764e02d184030e0d6963413b101960c4fce23
gxldnn <riberajan@gmail.com>
2025-03-25 11:32:36 +0100

    Docker Push

--- ASGBD(M10)/training/d.sql
+++ ASGBD(M10)/training/d.sql
@@ -118,6 +118,6 @@ WHERE ventas = (SELECT MIN(ventas) FROM repventa)
 --15. Quin producte (Descripció) té més comandes?
 
 SELECT descrip
-FROM (SELECT des, COUNT(prodcod||fabcod) AS "count" FROM pedido GROUP BY prod) AS "subtaula"
+FROM (SELECT descrip, COUNT(prodcod||fabcod) AS "count" FROM pedido GROUP BY prod) AS "subtaula"
 WHERE MAX(count) > ALL (SELECT COUNT(prodcod||fabcod) FROM pedido)
 --16i. Quin producte s'ha venut més?

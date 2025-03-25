9a0250b5825578af9245f87402b3dd398bda72f2
gxldnn <riberajan@gmail.com>
2025-03-25 10:22:54 +0100

    Docker Push

--- ASGBD(M10)/training/d.sql
+++ ASGBD(M10)/training/d.sql
@@ -119,6 +119,6 @@ WHERE ventas = (SELECT MIN(ventas) FROM repventa)
 
 SELECT prodcod||fabcod
 FROM producto
-WHERE prodcod||fabcod = (SELECT prodcod||fabcod FROM )
+WHERE prodcod||fabcod = (SELECT prodcod||fabcod FROM pedid)
 
 --16 . Quin producte s'ha venut més?

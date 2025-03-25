9520c791e0a0afe89565872ee67c2d77565af0f6
gxldnn <riberajan@gmail.com>
2025-03-25 09:48:47 +0100

    Docker Push

--- ASGBD(M10)/training/d.sql
+++ ASGBD(M10)/training/d.sql
@@ -1,18 +1,18 @@
 --0. Mostrar el nom i el lloc dels que són cap (ja està fet amb self join, ara amb subconsultes)
-/*
+
 SELECT nombre, puesto
 FROM repventa
 WHERE repcod IN (SELECT jefe FROM repventa);
 --1\. Obtenir una llista dels representants les quotes dels quals són iguals o
 --superiors a lobjectiu de loficina dAtlanta.
-/*
+
 SELECT nombre, cuota
 FROM repventa
 WHERE cuota >= ANY (SELECT objetivo FROM oficina);
 */
 --2\. Obtenir una llista de tots els clients (nom) que van ser
 --contactats per primera vegada per Bill Adams.
-/*
+
 SELECT nombre
 FROM cliente
 WHERE repcod = (SELECT repcod FROM repventa WHERE nombre = 'Bill Adams');
@@ -21,21 +21,21 @@ WHERE repcod = (SELECT repcod FROM repventa WHERE nombre = 'Bill Adams');
 --3\. Obtenir una llista de tots els productes del fabricant ACI les del qual
 --existències superen les existències del producte 41004 del mateix
 --fabricant.
-/*
+
 SELECT *
 FROM producto
 WHERE fabcod = 'aci' AND exist > (SELECT exist FROM producto WHERE prodcod = '41004');
 */
 --4\. Obtenir una llista dels representants que treballen a les oficines
 --que han aconseguit superar el seu objectiu de vendes.
-/*
+
 SELECT *
 FROM repventa
 WHERE ventas > ANY (SELECT objetivo FROM oficina);
 */
 --5\. Obtenir una llista dels representants que no treballen a les
 --oficines dirigides per Larry Fitch.
-/*
+
 SELECT *
 FROM repventa
 WHERE ofinum NOT IN (SELECT o.ofinum FROM oficina o JOIN repventa r ON r.repcod = o.director WHERE r.nombre = 'Larry Fitch');
@@ -45,21 +45,21 @@ WHERE ofinum NOT IN (SELECT o.ofinum FROM oficina o JOIN repventa r ON r.repcod 
 
 
 --INSERT INTO pedido VALUES (113015, '2003-01-11', 2111, 105, 'aci', 41003, 35, 3745.00); INSERT PER A QUE HI HAGI UN VALOR QUE BUSCAR
-/*
+
 SELECT * 
 FROM cliente
 WHERE cliecod IN (SELECT cliecod FROM pedido WHERE fabcod = 'aci' AND fecha BETWEEN '2003-01-01' AND '2003-06-30');
 */
 --7\. Obtenir una llista dels productes dels quals s'ha pres una comanda
 --de 150 euros o més.
-/*
+
 SELECT *
 FROM producto
 WHERE fabcod||prodcod IN (SELECT fabcod||prodcod FROM pedido WHERE importe > 150);
 */
 --8\. Obtenir una llista dels clients contactats per Sue Smith que no
 --han sol·licitat comandes amb imports superiors a 18 euros.
-/*
+
 SELECT *
 FROM cliente 
 WHERE repcod IN 
@@ -71,7 +71,7 @@ WHERE repcod IN
 --9\. Obtenir una llista de les oficines on hi hagi algun representant
 --la quota del qual sigui més del 55% de l'objectiu de l'oficina. Per comprovar el vostre
 --exercici, feu una Consulta prèvia el resultat de la qual valideu l'exercici.
-/*
+
 SELECT *
 FROM oficina
 WHERE ofinum IN 
@@ -80,7 +80,7 @@ WHERE ofinum IN
 
 --10\. Obtenir una llista dels representants que han pres alguna comanda
 --l'import del qual sigui més del 10% de la seva quota.
-/* 
+ 
 SELECT *
 FROM repventa r
 WHERE 0.1*cuota > ANY (SELECT importe FROM pedido p WHERE p.repcod = r.repcod);

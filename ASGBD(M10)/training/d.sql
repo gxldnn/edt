a30a97c9d6f5eb4848632d1136d54c25ddcf12f3
gxldnn <riberajan@gmail.com>
2025-03-25 10:15:47 +0100

    Docker Push

--- ASGBD(M10)/training/d.sql
+++ ASGBD(M10)/training/d.sql
@@ -101,7 +101,7 @@ WHERE 0.5 * o.objetivo < ANY (SELECT SUM(ventas) FROM repventa r WHERE r.ofinum 
 
 SELECT descrip
 FROM producto
-WHERE 
+WHERE por
 
 --13\. Quin representant té el millor percentatge de vendes?
 

267d63c77ed648d3d7af6e9025ba4c903a9a004f
gxldnn <riberajan@gmail.com>
2025-03-26 13:40:11 +0100

    Docker Push

--- ASGBD(M10)/training/a241477jr.sql
+++ ASGBD(M10)/training/a241477jr.sql
@@ -1,5 +1,5 @@
 --1. 
-/*
+/
 SELECT p.codpeli, l.coddvd, p.titol, s.nom||' '||s.cognoms "Nom Soci", l.datapres
 FROM lloguer l
 JOIN soci s ON l.codsoci = s.codsoci
@@ -9,16 +9,16 @@ WHERE CURRENT_DATE BETWEEN datapres AND datadev;
 */
 
 --2.
-/*
+
 SELECT s.codsoci "Codi Soci", s.cognoms||', '||s.nom "Soci",COUNT(s.codsoci) "Prestecs Realitzats", SUM(import) "Despesa"
 FROM soci s
 JOIN lloguer l ON l.codsoci = s.codsoci
 GROUP BY s.codsoci
 ORDER BY 4 DESC, 3,2;
-*/
+
 
 --3.
-/*
+
 SELECT DISTINCT genere
 FROM genere g
 JOIN pelicula p ON p.codgen = g.codgen
@@ -28,4 +28,3 @@ WHERE p.codpeli NOT IN (SELECT p.codpeli
     JOIN dvd d ON d.codpeli = p.codpeli
     JOIN lloguer l ON l.coddvd = d.coddvd)
 ORDER BY 1;
-*/

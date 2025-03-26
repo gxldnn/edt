d3fb3a13f8939b4476bc4195d64f743bfc19d296
gxldnn <riberajan@gmail.com>
2025-03-26 13:40:14 +0100

    Docker Push

--- ASGBD(M10)/training/a241477jr.sql
+++ ASGBD(M10)/training/a241477jr.sql
@@ -1,12 +1,12 @@
 --1. 
-/
+
 SELECT p.codpeli, l.coddvd, p.titol, s.nom||' '||s.cognoms "Nom Soci", l.datapres
 FROM lloguer l
 JOIN soci s ON l.codsoci = s.codsoci
 JOIN dvd d ON l.coddvd = d.coddvd
 JOIN pelicula p ON d.codpeli = p.codpeli
 WHERE CURRENT_DATE BETWEEN datapres AND datadev;
-*/
+
 
 --2.
 

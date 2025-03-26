20f6ea514df9af55081320534bd015b22b363bd5
gxldnn <riberajan@gmail.com>
2025-03-26 13:18:20 +0100

    Docker Push

--- ASGBD(M10)/training/a241477jr.sql
+++ ASGBD(M10)/training/a241477jr.sql
@@ -19,10 +19,4 @@ ORDER BY 4 DESC, 3,2;
 
 --3.
 
-SELECT (FROM genere 
-    SELECT p.codpeli, p.codgen, g.codgen, g.genere
-    FROM genere g
-    JOIN pelicula p ON g.codgen = p.codgen 
-    JOIN dvd d ON d.codpeli = p.codpeli
-    JOIN lloguer l ON l.coddvd = d.coddvd)
-    
+SELECT ()

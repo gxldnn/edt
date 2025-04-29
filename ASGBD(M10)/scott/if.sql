c640d27241ba31395aa97a1ca17773ffcd201c53
gxldnn <riberajan@gmail.com>
2025-04-29 13:31:51 +0200

    Docker Push

--- ASGBD(M10)/scott/if.sql
+++ ASGBD(M10)/scott/if.sql
@@ -9,6 +9,7 @@ CREATE OR REPLACE FUNCTION mostrar(p_empno SMALLINT){
             
             EXCEPTION
                 WHEN NO DATA FOUND THEN
+                        RETURN 'No s''ha trobat cap empleat amb aquest número.';
         END;
     $$ LANGUAGE PLPGSQL;
 }

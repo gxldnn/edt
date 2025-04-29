25a05c96b0fa207af51bc262569d4b985872f381
gxldnn <riberajan@gmail.com>
2025-04-29 13:30:56 +0200

    Docker Push

--- ASGBD(M10)/scott/if.sql
+++ ASGBD(M10)/scott/if.sql
@@ -1,11 +1,3 @@
-/*
-
-mostrar nom, salari, salari incremental i job segobs empno
-SALESMAN +10%
-CLERK +20%
-OTHERS +30%
-*/
-
 CREATE OR REPLACE FUNCTION mostrar(p_empno SMALLINT){
     RETURNS varchar
     AS $$

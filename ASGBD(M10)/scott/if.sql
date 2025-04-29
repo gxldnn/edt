df17843241aaba1f817eafc307940c474b9d4f62
gxldnn <riberajan@gmail.com>
2025-04-29 13:29:48 +0200

    Docker Push

--- ASGBD(M10)/scott/if.sql
+++ ASGBD(M10)/scott/if.sql
@@ -16,5 +16,5 @@ CREATE OR REPLACE FUNCTION mostrar(p_empno SMALLINT){
             SELECT * INTO STRICT v_emp FROM emp WHERE empno = p_empno
             
             EXCEPTION
-                WHEN NO DATA FO
+                WHEN NO DATA FOUND THE
 }

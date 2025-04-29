269e31bc4494bab0d86bcaab039e54e411ac8b0b
gxldnn <riberajan@gmail.com>
2025-04-29 13:25:24 +0200

    Docker Push

--- ASGBD(M10)/scott/if.sql
+++ ASGBD(M10)/scott/if.sql
@@ -10,7 +10,7 @@ CREATE OR REPLACE FUNCTION mostrar(){
     RETURNS v_ename varchar(100)
     AS $$
     DECLARE
-        v_ename varchar;
+        v_ename varchar(100);
         sal_sales SMALLINT;
         sal_clerk SMALLINT;
         sal_others SMALLINT;

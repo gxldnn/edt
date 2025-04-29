d4851ffc1952a9d24160beeef93414504985ada3
gxldnn <riberajan@gmail.com>
2025-04-29 13:23:37 +0200

    Docker Push

--- ASGBD(M10)/scott/if.sql
+++ ASGBD(M10)/scott/if.sql
@@ -15,7 +15,7 @@ CREATE OR REPLACE FUNCTION mostrar(){
         sal_clerk SMALLINT
         sal_others SMALLINT
     BEGIN
-        SELECT ename FROM emp;
+        SELECT ename INTO V FROM emp;
         SELECT sal * 1.1 INTO sal_sales FROM emp WHERE job = 'SALESMAN';
         SELECT sal * 1.2 INTO sal_clerk FROM emp WHERE job = 'CLERK';
         SELECT sal * 1.3 INTO sal_others FROM emp WHERE job != 'SALESMAN' OR job != 'CLERK';

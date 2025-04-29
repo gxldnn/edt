25ca2fcbc95d98cfd96977bf912060cf5adc1e7e
gxldnn <riberajan@gmail.com>
2025-04-29 13:28:49 +0200

    Docker Push

--- ASGBD(M10)/scott/if.sql
+++ ASGBD(M10)/scott/if.sql
@@ -13,6 +13,6 @@ CREATE OR REPLACE FUNCTION mostrar(){
             v_emp RECORD;
             v_sal emp.sal%type;
         BEGIN
-            SELECT * INTO STRICT v_emp FROM emp W
+            SELECT * INTO STRICT v_emp FROM emp WHERE empno =
 
 }

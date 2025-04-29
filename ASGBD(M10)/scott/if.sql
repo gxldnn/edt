188d2a122b4edf688b1a10bafc5c37c11a45c7be
gxldnn <riberajan@gmail.com>
2025-04-29 13:37:17 +0200

    Docker Push

--- ASGBD(M10)/scott/if.sql
+++ ASGBD(M10)/scott/if.sql
@@ -10,7 +10,7 @@ CREATE OR REPLACE FUNCTION mostrar(p_empno SMALLINT)
                 v_sal := v_emp.sal*1.1;
             ELSE IF LOWER(v_emp.job) = 'clerk' THEN
                 v_sal := v_emp.sal*1.2;
-            ELSE v_sa
+            ELSE v_sal := 
 
             RETURN v_sal;
             

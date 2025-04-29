de547d5e587da0fa64b08a9f795aa5fa0c9eddd7
gxldnn <riberajan@gmail.com>
2025-04-29 13:27:33 +0200

    Docker Push

--- ASGBD(M10)/scott/if.sql
+++ ASGBD(M10)/scott/if.sql
@@ -9,17 +9,3 @@ OTHERS +30%
 CREATE OR REPLACE FUNCTION mostrar(){
     RETURNS v_ename varchar(100)
     AS $$
-    DECLARE
-        v_ename varchar(100);
-        sal_sales SMALLINT;
-        sal_clerk SMALLINT;
-        sal_others SMALLINT;
-    BEGIN
-        SELECT ename INTO v_ename FROM emp;
-        SELECT sal * 1.1 INTO sal_sales FROM emp WHERE job = 'SALESMAN';
-        SELECT sal * 1.2 INTO sal_clerk FROM emp WHERE job = 'CLERK';
-        SELECT sal * 1.3 INTO sal_others FROM emp WHERE job != 'SALESMAN' OR job != 'CLERK';
-        RETURN v_ename, sal_sales, sal_clerk, sal_others;
-    END;
-    $$ LANGUAGE PLPGSQL;
-}

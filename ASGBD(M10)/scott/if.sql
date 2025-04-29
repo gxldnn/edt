18e3c3f589d91962cbd8c5d25bc6b6449f191dca
gxldnn <riberajan@gmail.com>
2025-04-29 13:40:46 +0200

    Docker Push

--- ASGBD(M10)/scott/if.sql
+++ ASGBD(M10)/scott/if.sql
@@ -8,8 +8,9 @@ CREATE OR REPLACE FUNCTION mostrar(p_empno SMALLINT)
             SELECT * INTO STRICT v_emp FROM emp WHERE empno = p_empno;  
             IF LOWER(v_emp.job = 'salesman') THEN
                 v_sal := v_emp.sal*1.1;
-            ELSEIF LOWER(v_emp.job) = 'clerk' THEN
-                v_sal := v_emp.sal*1.2;
+            ELSE
+                IF LOWER(v_emp.job) = 'clerk' THEN
+                    v_sal := v_emp.sal*1.2;
             ELSE v_sal := v_emp.sal*1.3;
             END IF;
             RETURN v_sal;

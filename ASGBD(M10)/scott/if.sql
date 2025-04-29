dae649014052971f5fcf7dd758ff21ee78758866
gxldnn <riberajan@gmail.com>
2025-04-29 13:31:59 +0200

    Docker Push

--- ASGBD(M10)/scott/if.sql
+++ ASGBD(M10)/scott/if.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE FUNCTION mostrar(p_empno SMALLINT){
             v_sal emp.sal%type;
         BEGIN
             SELECT * INTO STRICT v_emp FROM emp WHERE empno = p_empno;
-            
+      
             EXCEPTION
                 WHEN NO DATA FOUND THEN
                  RETURN 'No s''ha trobat cap empleat amb aquest número.';

b37607cd4cdf241af5b697c9ee4cd30143513ba9
gxldnn <riberajan@gmail.com>
2025-05-06 12:13:33 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -76,7 +76,7 @@ CREATE OR REPLACE FUNCTION altaComanda(p_cliecod INT, p_cant INT, p_fabcod varch
                 p_cant,
                 v_importe
             );
-            RETURN format('Una quantit')
+            RETURN format('Una quantitat de ')
 
             INSERT INTO pedido ()
         END;

f83e6ee709337706d7418850ccb6b77f8bbfd202
gxldnn <riberajan@gmail.com>
2025-05-06 12:12:08 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -63,7 +63,7 @@ CREATE OR REPLACE FUNCTION altaComanda(p_cliecod INT, p_cant INT, p_fabcod varch
             END IF;
 
             SELECT precio*p_cant FROM producto
-            WHERE fabcod||prodcod = fabcod||prodcod
+            WHERE fabcod||prodcod = fabcod||p_prodcod
             
             
             INSERT INTO pedido (pednum, fecha, cliecod, fabcod, prodcod, cant, importe)

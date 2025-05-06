51273cd0fe457a7221470beb753a401e14d7decd
gxldnn <riberajan@gmail.com>
2025-05-06 11:51:05 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -28,6 +28,6 @@ CREATE OR REPLACE FUNCTION stock0k(p_cant INT, p_fabcod INT, p_prodcod INT)
     AS $$
         DECLARE
         v_bool boolean;
-        v_exist producto.e
+        v_exist producto.exist
         BEGIN
             SELECT exist INTO STRICT v_exist

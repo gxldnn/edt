2714053281eedbaea8906d6aafadeef73086e56c
gxldnn <riberajan@gmail.com>
2025-05-06 13:39:06 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -32,7 +32,7 @@ CREATE OR REPLACE FUNCTION altaClient(p_nombre CHAR,p_repcod INT,p_limcred INT)
         
             EXCEPTION
                 WHEN NO_DATA_FOUND THEN
-                    RETURN 'Errot '
+                    RETURN 'Er'
         END;
     $$ LANGUAGE PLPGSQL;
 

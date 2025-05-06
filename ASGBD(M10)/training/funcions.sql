a3a11e10755596fea3ae5b8f5c3ed0c0433d9012
gxldnn <riberajan@gmail.com>
2025-05-06 12:58:24 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -54,32 +54,39 @@ AS $$
     BEGIN
         SELECT CURRENT_DATE INTO v_data;
 
+        -- Assignar el següent valor de la seqüència
         SELECT setval('pednum_seq', (SELECT max(pednum) FROM pedido) + 1, true) INTO v_pednum;
 
+        -- Comprovar si el client existeix
         IF NOT existeixClient(p_cliecod::SMALLINT) THEN
-            RETURN format('Client %s no existeix, no es pot fer la comanda', p_cliecod);
+            RETURN format('Client %s no existeix, no es pot fer la comanda', p_cliecod::text);
         END IF;
 
+        -- Comprovar si hi ha estoc disponible
         IF NOT stock0k(p_cant, p_fabcod, p_prodcod) THEN
-            RETURN format('No hi han existències suficients del producte %s', p_fabcod || p_prodcod);
+            RETURN format('No hi han existències suficients del producte %s', (p_fabcod || p_prodcod)::text);
         END IF;
 
+        -- Calcular l'import de la comanda
         SELECT precio * p_cant INTO v_importe
         FROM producto
-        WHERE fabcod||prodcod = p_fabcod||p_prodcod;
+        WHERE fabcod || prodcod = p_fabcod || p_prodcod;
         
+        -- Comprovar si el producte existeix i si l'import és vàlid
         IF NOT FOUND OR v_importe IS NULL THEN
-            RETURN format('No s’ha pogut calcular l’import: producte %s no trobat o preu nul', p_fabcod||p_prodcod);
+            RETURN format('No s’ha pogut calcular l’import: producte %s no trobat o preu nul', (p_fabcod || p_prodcod)::text);
         END IF;
         
-        
+        -- Inserir la comanda a la taula "pedido"
         INSERT INTO pedido
         VALUES (v_pednum, v_data, p_cliecod, NULL, p_fabcod, p_prodcod, p_cant, v_importe);
+
+        -- Retornar el missatge de confirmació
         RETURN format('Una quantitat de %s del producte %s amb un import de %s € s’ha afegit a pedidos pel client %s',
             p_cant,
-            p_prodcod || p_fabcod,
+            (p_prodcod || p_fabcod)::text,
             v_importe,
-            p_cliecod
+            p_cliecod::text
         );
     END;
 $$ LANGUAGE plpgsql;

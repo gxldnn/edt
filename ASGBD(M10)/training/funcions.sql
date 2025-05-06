672d5134c8520e96cfdd7f66a14043815a5055ba
gxldnn <riberajan@gmail.com>
2025-05-06 12:05:07 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -58,4 +58,4 @@ CREATE OR REPLACE FUNCTION altaComanda(p_cliecod INT, p_cant INT, p_prodcod INT,
                 RETURN format('No hi han existencies suficients del producte $s', p_fabcod||p_prodcod );
             END IF;
         END;
-    $$ LANGUAGE PLP
+    $$ LANGUAGE PLPGSQL;

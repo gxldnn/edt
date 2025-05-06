bba18a946778516cdf389226a0079d6e8b47c62e
gxldnn <riberajan@gmail.com>
2025-05-06 12:55:14 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -76,6 +76,6 @@ AS $$
         INSERT INTO pedido
         VALUES (v_pednum, v_data, p_cliecod, NULL, p_fabcod, p_prodcod, p_cant, v_importe);
 
-        RETURN format('ask %s', p_cliecod);
+        RETURN format('ask %s', );
     END;
 $$ LANGUAGE plpgsql;

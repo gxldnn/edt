9e64d7d8ef60752b6b443c2e24691a84ec20cdd6
gxldnn <riberajan@gmail.com>
2025-05-06 11:42:07 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -17,4 +17,4 @@ CREATE OR REPLACE FUNCTION altaClient(p_nombre::CHAR,p_repcod::INT,p_limcred::IN
             v_cliecod cliente.cliecod%type;
         BEGIN
             select setval('cliecod_seq', (select max(cliecod) from cliente), true) INTO STRICT v_cliecod;
-            INSERT INTO cliente (cliecod, nombre, repcod, limcred) VALUES (NEXTVAL('cliecod_seq'), p_nombre, p_repcod, p_limcred);
+            INSERT INTO cliente (cliecod, nombre, repcod, limcred) VALUES (NEXTVAL('cliecod_seq, p_nombre, p_repcod, p_limcred);

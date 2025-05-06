6ddda577a0e5b294fbf869dd614f1dfeaa6f8745
gxldnn <riberajan@gmail.com>
2025-05-06 11:53:21 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -38,5 +38,6 @@ CREATE OR REPLACE FUNCTION stock0k(p_cant INT, p_fabcod INT, p_prodcod INT)
             ELSE
                 RETURN TRUE;
             ENDIF;
-        
+        END;
+    
           

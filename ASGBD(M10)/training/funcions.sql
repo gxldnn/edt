37604ac07cf2d6482ec8ed24c45d69795bbee931
gxldnn <riberajan@gmail.com>
2025-05-06 13:17:44 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -73,7 +73,7 @@ AS $$
             RETURN format('No sha pogut calcular limport producte %s no trobat o preu nul', p_fabcod||p_prodcod);
         END IF;
         
-        */
+        
         INSERT INTO pedido
         VALUES (v_pednum, v_data, p_cliecod, NULL, p_fabcod, p_prodcod, p_cant, v_importe);
         RETURN format('Una quantitat de %s del producte %s amb un import de %s € sha afegit a pedidos pel client %s',

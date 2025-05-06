3dd5a4046a375ca09a1ab0c469770465872ffbfe
gxldnn <riberajan@gmail.com>
2025-05-06 11:31:19 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -5,4 +5,5 @@ CREATE OR REPLACE FUNCTION existeixClient(p_cliecod)
             v_bool boolean;
         BEGIN
             SELECT cliecod INTO STRICT v_bool FROM cliente WHERE cliecod = p_cliecod;
-            RETURNS v_bool; 
+            RETURNS v_bool;
+            

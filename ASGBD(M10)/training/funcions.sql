9460270c4d3a74647d0b80559fd7970e05527260
gxldnn <riberajan@gmail.com>
2025-05-06 11:54:12 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -35,8 +35,8 @@ CREATE OR REPLACE FUNCTION stock0k(p_cant INT, p_fabcod INT, p_prodcod INT)
 
             IF p_cant < v_exist THEN
                 RETURN FALSE;
-            ELSE 
-                RETURN TRUE;
+                ELSE 
+                    RETURN TRUE;
             ENDIF;
         END;
     $$ LANGUAGE PLPGSQL;

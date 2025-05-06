e26180ec691372389287a6236c0f070d5bb62a4f
gxldnn <riberajan@gmail.com>
2025-05-06 12:08:05 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -49,9 +49,10 @@ CREATE OR REPLACE FUNCTION altaComanda(p_cliecod INT, p_cant INT, p_fabcod varch
     AS $$
         DECLARE
             v_data pedido.fecha%type;
+            
         BEGIN
             SELECT CURRENT_DATE INTO STRICT v_data;
-            select setval('pednum_seq', (select max(pednum) from pedido)+1, true) INTO STRICT v_pe;
+            select setval('pednum_seq', (select max(pednum) from pedido)+1, true) INTO STRICT v_pednum;
 
             IF existeixClient(p_cliecod) == false THEN
                 RETURN format('Client %s no existeix, no es pot fer la comanda',p_cliecod);

ff83eed3baebccaddcdf9fa0f2ad8e3c11a41f0a
gxldnn <riberajan@gmail.com>
2025-05-06 13:00:59 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -67,7 +67,7 @@ AS $$
         SELECT precio * p_cant INTO v_importe
         FROM producto
         WHERE fabcod||prodcod = p_fabcod||p_prodcod;
-        
+        /*
         IF NOT FOUND OR v_importe IS NULL THEN
             RETURN format('No s’ha pogut calcular l’import producte %s no trobat o preu nul', p_fabcod||p_prodcod);
         END IF;

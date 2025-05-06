70455b53b48de47671c851e4d4370bdfc4cebce5
gxldnn <riberajan@gmail.com>
2025-05-06 12:12:22 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -62,7 +62,7 @@ CREATE OR REPLACE FUNCTION altaComanda(p_cliecod INT, p_cant INT, p_fabcod varch
                 RETURN format('No hi han existencies suficients del producte $s', p_fabcod||p_prodcod );
             END IF;
 
-            SELECT precio*p_cant INTO ST FROM producto
+            SELECT precio*p_cant INTO STRICT FROM producto
             WHERE fabcod||prodcod = p_fabcod||p_prodcod
             
             

9d3f4d90b8a95bd4ea012330cea33ab105734128
gxldnn <riberajan@gmail.com>
2025-05-06 12:23:52 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -44,8 +44,7 @@ CREATE OR REPLACE FUNCTION stock0k(p_cant INT, p_fabcod char, p_prodcod char)
         END; 
     $$ LANGUAGE PLPGSQL;
           
-CREATE OR REPLACE FUNCTION crear_comanda(
-    p_cliecod int,
+CREATE OR REPLACE FUNCTION crear_comanda(p_cliecod int,
     p_fabcod varchar,
     p_prodcod varchar,
     p_cant int)

8fafa35f8421ca138c05025ec3bf7bac8fa93788
gxldnn <riberajan@gmail.com>
2025-05-06 11:33:41 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -5,6 +5,8 @@ CREATE OR REPLACE FUNCTION existeixClient(p_cliecod SMALLINT)
             v_bool boolean;
         BEGIN
             SELECT cliecod INTO STRICT v_bool FROM cliente WHERE cliecod = p_cliecod;
+            
+            
             RETURN v_bool;
         END;
     $$ LANGUAGE PLPGSQL;

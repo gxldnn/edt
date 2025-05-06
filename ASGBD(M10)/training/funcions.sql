84733a938a9622894191e3128dff936405829cbc
gxldnn <riberajan@gmail.com>
2025-05-06 13:41:25 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -85,7 +85,7 @@ AS $$
         
         INSERT INTO pedido
         VALUES (NEXTVAL('pednum_seq'), v_data, p_cliecod, NULL, p_fabcod, p_prodcod, p_cant, v_importe);
-        RETURN format('Una quantitat de %s del producte %s amb un import de %s € sha afegit a pedidos pel client %s'
+        RETURN format('Una quantitat de %s del producte %s amb un import de %s € sha afegit a pedidos pel client %s';
         
     END;
 $$ LANGUAGE plpgsql;

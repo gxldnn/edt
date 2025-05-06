9664199423553c2ba064a2b6034f52ea5a4a9023
gxldnn <riberajan@gmail.com>
2025-05-06 13:40:27 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -71,7 +71,7 @@ AS $$
         END IF;
 
         IF NOT stock0k(p_cant, p_fabcod, p_prodcod) THEN
-            RETURN 'No hi han existències suficients del producte '||fabco||'';
+            RETURN 'No hi han existències suficients del producte '||fabcod||||'';
         END IF;
 
         SELECT precio * p_cant INTO v_importe

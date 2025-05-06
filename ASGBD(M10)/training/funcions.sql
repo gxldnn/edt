c38b52c13f29eb4f2ff2f826166f4ce2037d6696
gxldnn <riberajan@gmail.com>
2025-05-06 13:15:39 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -44,3 +44,44 @@ CREATE OR REPLACE FUNCTION stock0k(p_cant INT, p_fabcod char, p_prodcod char)
         END; 
     $$ LANGUAGE PLPGSQL;
         
+      
+CREATE OR REPLACE FUNCTION altaComanda(p_cliecod int, p_fabcod char, p_prodcod char, p_cant int)
+RETURNS varchar 
+AS $$
+    DECLARE
+        v_data pedido.fecha%type;
+        v_pednum pedido.pednum%type;
+        v_importe pedido.importe%type;
+    BEGIN
+        SELECT CURRENT_DATE INTO v_data;
+
+        SELECT setval('pednum_seq', (SELECT max(pednum) FROM pedido) + 1, true) INTO v_pednum;
+
+        IF NOT existeixClient(p_cliecod::SMALLINT) THEN
+            RETURN format('Client %s no existeix, no es pot fer la comanda', p_cliecod);
+        END IF;
+
+        IF NOT stock0k(p_cant, p_fabcod, p_prodcod) THEN
+            RETURN format('No hi han existències suficients del producte %s', p_fabcod||p_prodcod);
+        END IF;
+
+        SELECT precio * p_cant INTO v_importe
+        FROM producto
+        WHERE fabcod||prodcod = p_fabcod||p_prodcod;
+        
+        IF NOT FOUND OR v_importe IS NULL THEN
+            RETURN format('No s’ha pogut calcular l’import producte %s no trobat o preu nul', p_fabcod||p_prodcod);
+        END IF;
+        
+        
+        INSERT INTO pedido
+        VALUES (v_pednum, v_data, p_cliecod, NULL, p_fabcod, p_prodcod, p_cant, v_importe);
+        RETURN format('Una quantitat de %s del producte %s amb un import de %s € s’ha afegit a pedidos pel client %s',
+            p_cant,
+            p_prodcod || p_fabcod,
+            v_importe,
+            p_cliecod
+        );
+        
+    END;
+$$ LANGUAGE plpgsql;

c93ce9d0cc5dd2e195d1b6b9a44c3949bf5d6b7c
gxldnn <riberajan@gmail.com>
2025-05-06 13:37:39 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -22,7 +22,8 @@ CREATE OR REPLACE FUNCTION altaClient(p_nombre CHAR,p_repcod INT,p_limcred INT)
             v_repcod repventa.repcod%type;
         BEGIN
             SELECT repcod INTO STRICT v_repcod
-            FROM 
+            FROM repventa
+            WHERE 
 
             INSERT INTO cliente
             VALUES (NEXTVAL('cliecod_seq'), p_nombre, p_repcod, p_limcred);

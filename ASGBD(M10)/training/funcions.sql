1f2fe5b91c663af8b3090e96a1d8422c51add6d3
gxldnn <riberajan@gmail.com>
2025-05-06 12:53:11 +0200

    Docker Push

--- ASGBD(M10)/training/funcions.sql
+++ ASGBD(M10)/training/funcions.sql
@@ -77,10 +77,10 @@ AS $$
         VALUES (v_pednum, v_data, p_cliecod, NULL, p_fabcod, p_prodcod, p_cant, v_importe);
 
         RETURN format('Una quantitat de %s del producte %s amb un import de %s € s’ha afegit a pedidos pel client %s',
-            p_cant,
-            p_prodcod || p_fabcod,
-            v_importe,
-            p_cliecod
-        );
+    p_cant,
+    p_prodcod || p_fabcod,
+    v_importe,
+    p_cliecod
+);
     END;
 $$ LANGUAGE plpgsql;

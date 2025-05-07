be2ea0d7229d5b2dde558b330972a03b026ee4b1
gxldnn <riberajan@gmail.com>
2025-05-07 13:32:16 +0200

    Docker Push

--- ASGBD(M10)/installerdocker.sh
+++ ASGBD(M10)/installerdocker.sh
@@ -15,7 +15,7 @@ services:
       POSTGRES_DB: template1
     ports:
       - "5432:5432"
-    working_dir: /workspace/sql/edt/ASGBD()/
+    working_dir: /workspace/sql/
     volumes:
       - pgdata:/var/lib/postgresql/data
       - ./sql:/workspace/sql

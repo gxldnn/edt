b0bc980e718b41f1ce3ccdef18f1ad8b6786de70
gxldnn <riberajan@gmail.com>
2025-05-07 12:34:57 +0200

    Docker Push

--- ASGBD(M10)/training/funcionsTraining.sql
+++ ASGBD(M10)/training/funcionsTraining.sql
@@ -133,5 +133,5 @@ SELECT altaClient('Jose', 107, 4000);
 SELECT stock0k(40, 'rei'::varchar, '2a45c'::varchar);
 SELECT altaComanda(2111, 'rei'::varchar, '2a45c'::varchar, 20);
 SELECT preusenseIVA(56);
-SELECT preusensIVA(56);
+SELECT preuambIVA(56);
 

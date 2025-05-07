813c6dcf7f7a674c0369bc60cce4a5849e5c6297
gxldnn <riberajan@gmail.com>
2025-05-07 13:30:09 +0200

    Docker Push

--- ASGBD(M10)/installerdocker.sh
+++ ASGBD(M10)/installerdocker.sh
@@ -25,4 +25,14 @@ volumes:
 
 
 " >> /Documents/dockerpsql/docker-compose.yaml
-echo "" >> ~/.bashrc
+echo "
+function postgresdocker() {
+  docker stop $(docker ps -a -q)
+  docker remove $(docker ps -a -q)
+  cd ~/Documents/docker/postgres
+  docker compose down
+  docker compose up -d
+  id=$(docker ps | awk 'NR>1 {print $1}')
+  docker exec -it $id bash
+}
+" >> ~/.bashrc

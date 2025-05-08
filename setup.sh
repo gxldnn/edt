364e68df0a98b838e56ad3c708ee8d7cdb4f023f
gxldnn <riberajan@gmail.com>
2025-05-08 12:44:12 +0200

    Docker Push

--- setup.sh
+++ setup.sh
@@ -34,6 +34,14 @@ function postgresdocker() {
     docker compose up -d
     id=$(docker ps | awk 'NR>1 {print $1}')
     docker exec -it $id bash
+}function postgresdocker() {
+    docker stop $(docker ps -a -q)
+    docker rm $(docker ps -a -q)
+    cd ~/Documents/dockerpsql
+    docker compose down
+    docker compose up -d
+    id=$(docker ps | awk 'NR>1 {print $1}')
+    docker exec -it $id bash
 }
 
 function push() {

fd5da5207074354b74626426fe966ede3da788ce
gxldnn <riberajan@gmail.com>
2025-05-08 12:43:09 +0200

    Docker Push

--- setup.sh
+++ setup.sh
@@ -36,6 +36,37 @@ function postgresdocker() {
   docker exec -it $id bash
 }
 
+function push() {
+    clear
+
+    read -p "Clone Git?" gitclone
+    mkdir -p "~/Documents/docker/postgres/sql/"
+    cd ~/Documents/docker/postgres/sql/
+    case "$gitclone" in
+        "n" | "N" | "")
+            break
+            ;;
+        "y")
+            git clone git@github.com:gxldnn/edt.git
+            ;;
+        *)
+            exit 1
+            ;;
+    esac
+
+    cd edt/
+    git config --global pull.rebase false
+    git pull
+    clear
+
+    while [[ true ]]; do
+        git commit -m "Auto Push"
+        git add -A
+        git push
+        sleep 1
+        clear
+    done
+}
 
 
 

f88289b7c23a22346b31681f464426680380a7e1
gxldnn <riberajan@gmail.com>
2025-05-08 12:35:43 +0200

    Docker Push

--- setup.sh
+++ setup.sh
@@ -28,3 +28,42 @@ function push() {
     clear
   done
 }
+
+
+mkdir -p ~/Documents/dockerpsql
+mkdir -p ~/Documents/dockerpsql/sql
+
+cat << 'EOF' > ~/Documents/dockerpsql/docker-compose.yaml
+services:
+  postgres:
+    image: postgres:latest
+    command: >
+      bash -c "echo 'connect() { psql --host=localhost --username=$1 -d template1; }' >> /etc/bash.bashrc && exec docker-entrypoint.sh postgres"
+    container_name: postgres_server
+    environment:
+      POSTGRES_USER: admin
+      POSTGRES_PASSWORD: 123456
+      POSTGRES_DB: template1
+    ports:
+      - "5432:5432"
+    working_dir: /workspace/sql/
+    volumes:
+      - pgdata:/var/lib/postgresql/data
+      - ./sql:/workspace/sql
+
+volumes:
+  pgdata:
+EOF
+
+cat << 'EOF' >> ~/.bashrc
+
+function postgresdocker() {
+  docker stop $(docker ps -a -q)
+  docker rm $(docker ps -a -q)
+  cd ~/Documents/dockerpsql
+  docker compose down
+  docker compose up -d
+  id=$(docker ps | awk 'NR>1 {print $1}')
+  docker exec -it $id bash
+}
+EOF

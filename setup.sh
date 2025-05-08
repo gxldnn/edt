85562cf0c09179df261272c6410fa1da66cb6683
gxldnn <riberajan@gmail.com>
2025-05-08 12:47:11 +0200

    Docker Push

--- setup.sh
+++ setup.sh
@@ -21,27 +21,8 @@ services:
 
 volumes:
   pgdata:
-EOFcat << 'EOF' > ~/Documents/docker/psql/docker-compose.yaml
-services:
-  postgres:
-    image: postgres:latest
-    command: >
-      bash -c "echo 'connect() { psql --host=localhost --username=$1 -d template1; }' >> /etc/bash.bashrc && exec docker-entrypoint.sh postgres"
-    container_name: postgres_server
-    environment:
-      POSTGRES_USER: admin
-      POSTGRES_PASSWORD: 123456
-      POSTGRES_DB: template1
-    ports:
-      - "5432:5432"
-    working_dir: /workspace/sql/
-    volumes:
-      - pgdata:/var/lib/postgresql/data
-      - ./sql:/workspace/sql
-
-volumes:
-  pgdata:
 EOF
+
 
 cp /etc/skel/.bashrc ~/
 cat << 'EOF' >> ~/.bashrc

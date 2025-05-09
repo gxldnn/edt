0b0ea2a9316d14a224bfd54165f758fdb8e45d21
gxldnn <riberajan@gmail.com>
2025-05-09 10:04:34 +0200

    Docker Push

--- ASGBD(M10)/training/funcionsTraining.sql
+++ ASGBD(M10)/training/funcionsTraining.sql
@@ -63,7 +63,7 @@ CREATE OR REPLACE FUNCTION stockOk(p_cant INT, p_fabcod varchar, p_prodcod varch
             ELSE
                 RETURN TRUE;
             END IF;*/
-            /*
+            
             EXCEPTION
                 WHEN NO_DATA_FOUND THEN
                     RETURN FALSE;

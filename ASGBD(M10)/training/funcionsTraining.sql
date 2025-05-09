91932f4fdc1cd6ab8a8f290ecd0aa804137c77fe
gxldnn <riberajan@gmail.com>
2025-05-09 10:05:43 +0200

    Docker Push

--- ASGBD(M10)/training/funcionsTraining.sql
+++ ASGBD(M10)/training/funcionsTraining.sql
@@ -64,8 +64,7 @@ CREATE OR REPLACE FUNCTION stockOk(p_cant INT, p_fabcod varchar, p_prodcod varch
             EXCEPTION
                 WHEN NO_DATA_FOUND THEN
                     RETURN FALSE;
-                    
-                    
+                
         END; 
     $$ LANGUAGE PLPGSQL;
 

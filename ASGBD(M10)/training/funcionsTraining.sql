e3ff1979c5d1319965b7d60290486d4446534545
gxldnn <riberajan@gmail.com>
2025-05-09 10:04:07 +0200

    Docker Push

--- ASGBD(M10)/training/funcionsTraining.sql
+++ ASGBD(M10)/training/funcionsTraining.sql
@@ -56,7 +56,7 @@ CREATE OR REPLACE FUNCTION stockOk(p_cant INT, p_fabcod varchar, p_prodcod varch
         BEGIN
             SELECT exist INTO STRICT v_exist 
             FROM producto 
-            WHERE fabcod||prodcod = p_fabcod||p_prodcod; AND p_cant <= (SELECT exist FROM producto WHERE );
+            WHERE fabcod||prodcod = p_fabcod||p_prodcod; AND p_cant <= (SELECT exist FROM producto WHERE v );
 /*
             IF p_cant > v_exist THEN
                 RETURN FALSE;

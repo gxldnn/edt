c34c1e4b20159aff34412d0e3cfc81b821b10532
a241477jr <a241477jr@j18.informatica.escoladeltreball.org>
2025-05-14 13:22:18 +0200

    Auto Push

--- ASGBD(M10)/biblio/funcionsBiblioteca.sql
+++ ASGBD(M10)/biblio/funcionsBiblioteca.sql
@@ -57,7 +57,7 @@ AS $$
         v_iddocument document.format%type;
         v_numdocs INT;
     BEGIN
-        SELECT COUNT() FROM prestecs
+        SELECT COUNT(idExemplar) FROM prestecs
         WHERE p_usuari = idUsuari
     END;
 

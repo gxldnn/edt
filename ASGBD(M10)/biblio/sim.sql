d54c09112473187eee2a05966feab28acd4d95ff
a241477jr <a241477jr@j18.informatica.escoladeltreball.org>
2025-05-14 12:46:29 +0200

    Auto Push

--- ASGBD(M10)/biblio/sim.sql
+++ ASGBD(M10)/biblio/sim.sql
@@ -16,7 +16,7 @@ AS $$
         
         IF estat = 'Discponible'
             RETURN v_isbn;
-
+        ELSE
             EXCEPTION
                 WHEN NO_DATA_FOUND THEN
                     RETURN 0;

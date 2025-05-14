ae7e01a3561190e1f08553223c4359868954c6ee
a241477jr <a241477jr@j18.informatica.escoladeltreball.org>
2025-05-14 13:18:52 +0200

    Auto Push

--- ASGBD(M10)/biblio/funcionsBiblioteca.sql
+++ ASGBD(M10)/biblio/funcionsBiblioteca.sql
@@ -49,4 +49,4 @@ AS $$
     END;
 $$ LANGUAGE PLPGSQL
 
-CREATE OR REPLACE FUNCTION documentsPrestats(p_usuari INT, p_format)
+CREATE OR REPLACE FUNCTION documentsPrestats(p_usuari INT, p_format varchar)

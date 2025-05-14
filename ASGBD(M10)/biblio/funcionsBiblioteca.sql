cf8107dbade8c0ec1cb6c1939bb62d6aa0dbf0c6
a241477jr <a241477jr@j18.informatica.escoladeltreball.org>
2025-05-14 13:33:16 +0200

    Auto Push

--- ASGBD(M10)/biblio/funcionsBiblioteca.sql
+++ ASGBD(M10)/biblio/funcionsBiblioteca.sql
@@ -53,7 +53,6 @@ CREATE OR REPLACE FUNCTION documentsPrestats(p_usuari INT, p_format varchar)
 RETURNS INT
 AS $$
     DECLARE
-        v_prestecusuari recor
         v_idusuari usuari.idUsuari%type;
         v_iddocument document.format%type;
         v_numdocs INT;

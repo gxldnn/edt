77efcbebbe5f3051d83cdcdf420987995566f8e5
a241477jr <a241477jr@j18.informatica.escoladeltreball.org>
2025-05-14 12:43:48 +0200

    Auto Push

--- ASGBD(M10)/biblio/sim.sql
+++ ASGBD(M10)/biblio/sim.sql
@@ -5,7 +5,7 @@ AS $$
         v_isbn varchar;
         v_iddocument int;
     BEGIN
-        SELECT ISBN INTO STRICT v_isbn, idDocument INTO S
+        SELECT ISBN INTO STRICT v_isbn, idDocument INTO STRICT
         FROM llibre
         WHERE LOWER(p_titol) = LOWER(titol);
 

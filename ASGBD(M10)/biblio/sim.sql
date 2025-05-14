4de5c64467b4f9a812a9d2b3dd4569eb1d40ba14
a241477jr <a241477jr@j18.informatica.escoladeltreball.org>
2025-05-14 12:58:23 +0200

    Auto Push

--- ASGBD(M10)/biblio/sim.sql
+++ ASGBD(M10)/biblio/sim.sql
@@ -10,7 +10,7 @@ AS $$
         FROM llibre
         WHERE LOWER(p_titol) = LOWER(titol);    
         
-        SELECT estat INTO STRICT v_estat FROM Exemplar
+        SELECT estat INTO STRICT v_estat FROM exemplar
         WHERE idDocument = v_iddocument;
 
 

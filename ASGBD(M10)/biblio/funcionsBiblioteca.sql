38ff6e21f7c9b12d5ced9935e4c3896708529f7c
a241477jr <a241477jr@j18.informatica.escoladeltreball.org>
2025-05-14 13:50:29 +0200

    Auto Push

--- ASGBD(M10)/biblio/funcionsBiblioteca.sql
+++ ASGBD(M10)/biblio/funcionsBiblioteca.sql
@@ -58,7 +58,7 @@ AS $$
         v_numdocs INT;
     BEGIN
         SELECT COUNT(idUsuari) INTO STRICT v_numdocs FROM prestec
-        
+        JOIN
         WHERE p_usuari = idUsuari;
 
         SELECT iddocument INTO STRICT v_iddocument FROM exemplar

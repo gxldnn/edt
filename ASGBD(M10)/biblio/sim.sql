19e6dcac7adc636fd84d584d939d0f98ca8db771
a241477jr <a241477jr@j18.informatica.escoladeltreball.org>
2025-05-14 13:12:45 +0200

    Auto Push

--- ASGBD(M10)/biblio/sim.sql
+++ ASGBD(M10)/biblio/sim.sql
@@ -32,7 +32,7 @@ CREATE OR REPLACE FUNCTION usuarioOK(p_codiusuari)
 RETURNS INT
 AS $$
     DECLARE
-        v_bloq usuari.bloquejat
+        v_bloq usuari.bloquejat%type
     BEGIN
     
     END;

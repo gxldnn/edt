768ecd05446e0ef11a9a4c90644172462d676ac3
gxldnn <riberajan@gmail.com>
2025-05-21 12:30:44 +0200

    Docker Push

--- ASGBD(M10)/scott/transaccions.sql
+++ ASGBD(M10)/scott/transaccions.sql
@@ -4,4 +4,8 @@ CREATE DATABASE scott;
 \i scott/scott.sql
 \c scott
 
-/*
+INSERT INTO punts (id, valor) VALUES (10,5);
+BEGIN;
+UPDATE punts SET valor = 4 WHERE id = 10;
+ROLLBACK;
+SELECT valor FROM punts WHERE id = 10;
